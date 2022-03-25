c770d473a48c5d494c326149abbc7c4c19638988
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-25 09:26:01 -0400

    Sistema BK

--- app/src/produtos/pagar_dinheiro.php
+++ app/src/produtos/pagar_dinheiro.php
@@ -46,7 +46,7 @@
 <div class="col" style="margin-bottom:60px;">
     <div class="row">
             <div class="col-12">
-                <div class="card text-white bg-warning mb-3" style="padding:20px;">
+                <div class="card text-white bg-danger mb-3" style="padding:20px;">
                     <p style="text-align:center">Efetue o pagamento diretamente no caixa ou solicite a comanda.</p>
                     <div style="margin-top:60px; margin-bottom:60px; text-align:center; font-size:100px;">
                         <i class="fa-solid fa-hand-holding-dollar"></i>
