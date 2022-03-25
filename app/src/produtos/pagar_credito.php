69fc63d7d6ebefc796de8433903a3a661447145e
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-24 23:35:22 -0400

    Sistema BK

--- app/src/produtos/pagar_credito.php
+++ app/src/produtos/pagar_credito.php
@@ -137,7 +137,7 @@
                         </div>
                     </div>
                 </div>
-                <button class="btn btn-primary btn-block btn-lg" id="Pagar">PAGAR R$ <?=number_format($d->total, 2, ',','.')?></button>
+                <button class="btn btn-secondary btn-block btn-lg" id="Pagar">PAGAR R$ <?=number_format($d->total, 2, ',','.')?></button>
             </div>
         </div>
     </div>
