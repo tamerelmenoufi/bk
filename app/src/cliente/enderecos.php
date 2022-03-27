f3fdf093648de9888d910048caf596fd45e6e9cf
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-26 20:43:41 -0400

    Sistema BK

--- app/src/cliente/enderecos.php
+++ app/src/cliente/enderecos.php
@@ -4,36 +4,59 @@
 
 <style>
 
-    #map {
+    #googleMap {
         position:absolute;
         left:0;
         top:0;
         height: 100%;
         width:100%;
+        z-index:0;
+    }
+    #menu{
+        position:fixed;
+        bottom:0;
+        width:100%;
+        background:#333;
+        opacity:0.5;
     }
 
 </style>
 
-<div id="map">MAPA NO AR Versão 2</div>
+<div id="googleMap"></div>
+<div id="menu">
+    <input type="text" id="adress" name="adress">
+    <input type="submit" class="btn" value="Search">
+</div>
 
 <script>
 
-function initMap() {
-  const cairo = { lat: 30.064742, lng: 31.249509 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    scaleControl: true,
-    center: cairo,
-    zoom: 10,
-  });
-  const infowindow = new google.maps.InfoWindow();
-
-  infowindow.setContent("<b>القاهرة</b>");
-
-  const marker = new google.maps.Marker({ map, position: cairo });
-
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
+function initialize()
+{
+var mapProp = {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+var map=new google.maps.Map(document.getElementById("googleMap")
+  ,mapProp);
 }
-initMap();
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+GMaps.geocode({
+  address: $('#adress').val(),
+  callback: function(results, status) {
+    if (status == 'OK') {
+      var latlng = results[0].geometry.location;
+      map.setCenter(latlng.lat(), latlng.lng());
+      map.addMarker({
+        lat: latlng.lat(),
+        lng: latlng.lng()
+      });
+    }
+  }
+});
+
+
 </script>
