5e72e00c894e90bf6adab43f15f7cd8c3b7c0435
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-27 10:56:28 -0400

    Sistema BK

--- app/src/cliente/enderecos.php
+++ app/src/cliente/enderecos.php
@@ -18,9 +18,7 @@
     <div id="map"></div>
 
     <script>
-        map = new google.maps.Map(
-            div: '#map',
-        {
+        map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: -34.397, lng: 150.644 },
             zoom: 8,
         });
