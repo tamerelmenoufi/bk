3d91a130bec0847c432342653632c0c7d9e8c12a
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-27 23:30:02 -0400

    Sistema BK

--- app/src/cliente/enderecos.php
+++ app/src/cliente/enderecos.php
@@ -4,70 +4,46 @@
 
 <style>
 
-    #map {
-        position:absolute;
-        left:0;
-        top:0;
-        height: 100%;
-        width:100%;
-        z-index:0;
-    }
+
 
 </style>
 
-    <div id="map"></div>
+<style>
+    .EnderecoTitulo{
+        position:fixed;
+        left:70px;
+        top:0px;
+        height:60px;
+        padding-top:15px;
+        z-index:1;
+    }
+
+</style>
+<div class="EnderecoTitulo">
+    <h4>Lista de Endereços</h4>
+</div>
+<div class="col" style="margin-bottom:60px;">
+    <div class="row">
+        <div class="col-12">
+            <?php
+                for($i=0;$i<5;$i++){
+            ?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-img-top" alt="Card image cap"></div>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <?php
+                }
+            ?>
+        </div>
+    </div>
+</div>
 
     <script>
 
-        endereco = "Rua Monsenhor Coutinho, 600, Centro, Manaus, Amazonas";
-        geocoder = new google.maps.Geocoder();
-        map = new google.maps.Map(document.getElementById("map"), {
-            zoomControl: false,
-            mapTypeControl: false,
-            draggable: true,
-            scaleControl: false,
-            scrollwheel: false,
-            navigationControl: false,
-            streetViewControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            fullscreenControl: false,
-        }
-        /*{
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8,
-        }*/
-        );
 
-        marker = new google.maps.Marker({
-            position: { lat: -34.397, lng: 150.644 },
-            map,
-            title: "Hello World!",
-            draggable:true,
-        });
-
-        google.maps.event.addListener(marker, 'dragend', function(marker) {
-            var latLng = marker.latLng;
-            alert(`Lat ${latLng.lat()} & Lng ${latLng.lng()}`)
-        });
-
-
-        geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, (results, status) => {
-
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-
-                    var latitude = results[0].geometry.location.lat();
-                    var longitude = results[0].geometry.location.lng();
-
-                    $('Endereco').val(results[0].formatted_address);
-
-                    var location = new google.maps.LatLng(latitude, longitude);
-                    marker.setPosition(location);
-                    map.setCenter(location);
-                    map.setZoom(16);
-                }
-            }
-        });
 
 
 </script>
