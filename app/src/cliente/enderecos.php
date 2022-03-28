24bb54be390b2dd677119808de5c603b27b68895
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-27 23:33:23 -0400

    Sistema BK

--- app/src/cliente/enderecos.php
+++ app/src/cliente/enderecos.php
@@ -17,7 +17,11 @@
         padding-top:15px;
         z-index:1;
     }
-
+    .mapa{
+        width:100%;
+        height:200px;
+        background-color:#ccc;
+    }
 </style>
 <div class="EnderecoTitulo">
     <h4>Lista de Endereços</h4>
@@ -28,8 +32,10 @@
             <?php
                 for($i=0;$i<5;$i++){
             ?>
-            <div class="card" style="width: 18rem;">
-                <div class="card-img-top" alt="Card image cap"></div>
+            <div class="card" style="margin-bottom:10px;">
+                <div class="card-img-top mapa">
+
+                </div>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
