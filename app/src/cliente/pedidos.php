026b6b2d0d3c56e61af478ecc3144b3ff9134cf2
tamerelmenoufi <tamer@mohatron.com.br>
2022-03-28 21:14:56 -0400

    Sistema BK

--- app/src/cliente/pedidos.php
+++ app/src/cliente/pedidos.php
@@ -17,23 +17,29 @@
 <div class="PedidosTitulo">
     <h4>Meus Pedidos</h4>
 </div>
+<div class="col">
+    <div class="row">
+        <div class="col">
 <?php
     $query = "select * from vendas where cliente = '{$_SESSION['AppCliente']}' and deletado != '1' and data_finalizacao > 0 order by data_finalizacao desc";
     $result = mysqli_query($con, $query);
     $n = mysqli_num_rows($result);
     while($d = mysqli_fetch_object($result)){
 ?>
-    <div class="card" style="margin-bottom:10px;">
-        <div class="card-body">
-            <p class="card-text">
-                Pedido: <?=str_pad($d->codigo, 6, "0", STR_PAD_LEFT)?><br>
-                Valor: R$ <?=$d->total?><br>
-                Data do Pedido: <?=$d->data_finalizacao?><br>
-                Forma de Pagamento: <?=strtoupper($d->forma_pagamento)?><br>
-                situação: <?=$d->situacao?>
-            </p>
-        </div>
-    </div>
+            <div class="card" style="margin-bottom:10px;">
+                <div class="card-body">
+                    <p class="card-text">
+                        Pedido: <?=str_pad($d->codigo, 6, "0", STR_PAD_LEFT)?><br>
+                        Valor: R$ <?=$d->total?><br>
+                        Data do Pedido: <?=$d->data_finalizacao?><br>
+                        Forma de Pagamento: <?=strtoupper($d->forma_pagamento)?><br>
+                        situação: <?=$d->situacao?>
+                    </p>
+                </div>
+            </div>
 <?php
     }
 ?>
+        </div>
+    </div>
+</div>
