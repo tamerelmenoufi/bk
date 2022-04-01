378aa02bb0d5c284ef13489d14ddbde642c85b5c
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-01 09:36:37 -0400

    Atualizações

--- painel/combos/imagem.php
+++ painel/combos/imagem.php
@@ -12,7 +12,7 @@
 ?>
 
 <div class="col">
-    <div class="row">
+    <div id="IdTeste" class="row">
     <?php
         foreach($img as $i => $icon){
     ?>
@@ -31,47 +31,14 @@
 
 
 
-
-    <div id="mydiv" style="background-color:red ;background-size: 100%;
-background-size :200px 200px;
-background-repeat: no-repeat;">
-<p>text!</p>
-<div id="canvas">
-<p>Canvas:</p>
-</div>
-
- <div style="width:200px; float:left" id="image">
- <p style="float:left">Image: </p>
- </div>
- <div style="float:left;margin-top: 120px;" class="return-data">
- </div>
-
-
-
 </div>
 
 <script>
 
-html2canvas([document.getElementById('mydiv')], {
-    onrendered: function (canvas) {
-
-        document.getElementById('canvas').appendChild(canvas);
-        var data = canvas.toDataURL('image/png');
-        //display 64bit imag
-        var image = new Image();
-        image.src = data;
-        document.getElementById('image').appendChild(image);
-        // AJAX call to send `data` to a PHP file that creates an PNG image from the dataURI string and saves it to a directory on the server
-        console.log('image');
-        console.log(image);
-        var file= dataURLtoBlob(data);
-        console.log('file');
-        console.log(file);
-        // Create new form data
-        var fd = new FormData();
-        fd.append("imageNameHere", file);
-
-   }
+html2canvas(document.getElementById('IdTeste')).then(function(canvas) {
+    //document.body.appendChild(canvas);
+    console.log(canvas);
 });
+
 
 </script>
