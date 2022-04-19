0c8602d6047f2d577b467d5184c76edbb65235d0
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-19 12:08:38 -0400

    Atualizações

--- app/src/produtos/produto.php
+++ app/src/produtos/produto.php
@@ -77,6 +77,7 @@
     .foto<?=$md5?> {
         background-size: 80%;
         background-position: center bottom;
+        background-repeat:no-repeat;
         border-top-left-radius: 5px;
         border-bottom-left-radius: 5px;
         background-color:#f5ebdc;
