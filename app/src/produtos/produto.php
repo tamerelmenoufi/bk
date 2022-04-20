6319018ca25ddae40ec799f5ac8347b779df5a34
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-20 08:29:33 -0400

    Atualizações

--- app/src/produtos/produto.php
+++ app/src/produtos/produto.php
@@ -190,7 +190,7 @@
                     </div>
                 </div>
 
-                <div class="row" style="position:fixed; top:250px; z-index:1; overflow:auto;">
+                <div class="row" style="position:fixed; top:250px; z-index:1; overflow:scroll;">
                         <div class="col">
                             <div class="card-body">
                                 <!-- <h5 class="card-title">
