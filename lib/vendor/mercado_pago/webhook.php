dbe39b9be280fb14b3d10d8d819f4563c060b044
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-25 12:27:25 -0400

    Atualizações

---
+++ lib/vendor/mercado_pago/webhook.php
@@ -0,0 +1,12 @@
+<?php
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+
+    if($_GET and !$_POST) $_POST = $_GET;
+
+    $dadosLog = print_r($_POST,true);
+
+
+    file_put_contents("logs/retorno_".date("YmdHis").".txt",$dadosLog);
