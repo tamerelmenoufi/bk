1a39977eada8b9b6591e7bd97face0259d084966
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-04 08:28:17 -0400

    Atualizações

--- lib/vendor/mercado_pago/classes.php
+++ lib/vendor/mercado_pago/classes.php
@@ -10,8 +10,14 @@
         // public $TOKEN = '2b4e31d3a75b429c9ef5fdd02f2b5c59';
 
         //produção
+        # TAMER
         //public $AccessTOKEN = 'TEST-7005694497942738-042513-1436ca13f7406f79cb64f3ec45899547-182791413';
-        public $AccessTOKEN = 'APP_USR-7005694497942738-042513-790803032b2264d9df4929b9668cae16-182791413';
+        // public $AccessTOKEN = 'APP_USR-7005694497942738-042513-790803032b2264d9df4929b9668cae16-182791413';
+
+        # SIstema BK
+        //public $AccessTOKEN = 'TEST-7005694497942738-042513-1436ca13f7406f79cb64f3ec45899547-182791413';
+        public $AccessTOKEN = 'APP_USR-1171310380547745-050412-8bbf342041fd8942296d2cdf12af10a2-182791413';
+
 
 
         public function Autenticacao($opc){
