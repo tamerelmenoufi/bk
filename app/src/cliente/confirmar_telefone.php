276ddda17d79def7f8e1748ee95b751ffe82b8a4
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-22 23:30:59 -0400

    Atualizações

--- app/src/cliente/confirmar_telefone.php
+++ app/src/cliente/confirmar_telefone.php
@@ -2,4 +2,29 @@
 
     include("../../../lib/includes.php");
 
-    echo "Dados da validação";
+?>
+
+<style>
+    .ClienteTopoTitulo{
+        position:relative;
+        width:100%;
+        text-align:center;
+    }
+</style>
+
+<div class="ClienteTopoTitulo">
+    <h4>
+        <i class="fa-solid fa-user"></i> Confirmar Telefone
+    </h4>
+</div>
+
+<div class="col">
+    <div class="col-12">
+        O seu telefone 92 991886570 informado no cadastro, precisa ser confirmado para liberar o seu cadastro.
+        Como deseja receber o código de confirmação?
+
+        <button class="sms btn btn-primary btn-block btn-lg">POR SMS</button>
+        <button class="whatsapp btn btn-primary btn-block btn-lg">POR WHATSAPP</button>
+
+    </div>
+</div>
