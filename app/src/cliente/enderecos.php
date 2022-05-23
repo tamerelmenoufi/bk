7f793dbf8b11d90e7b4d50f1e8ba49d0a757d97b
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-22 22:30:17 -0400

    Atualizações

--- app/src/cliente/enderecos.php
+++ app/src/cliente/enderecos.php
@@ -103,7 +103,7 @@
                             <li acao="mapa_editar" cod="<?=$d->codigo?>" class="list-group-item text-primary">
                                 <div class="row">
                                     <div class="col-1"><i class="fa-solid fa-map-location-dot"></i></div>
-                                    <div class="col-11">Editar o Mapa</div>
+                                    <div class="col-11">Editar o Mapa <i class="fa-solid fa-triangle-exclamation text-danger"></i></div>
                                 </div>
                             </li>
                             <li padrao cod="<?=$d->codigo?>" class="list-group-item text-primary">
