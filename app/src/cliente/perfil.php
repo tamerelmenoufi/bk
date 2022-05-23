ea0586749c0a00e7aaa900d1cbf0b33ff08b17a2
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-22 23:04:42 -0400

    Atualizações

--- app/src/cliente/perfil.php
+++ app/src/cliente/perfil.php
@@ -41,7 +41,7 @@
                         <?php
                         if(!$c->telefone_confirmado){
                         ?>
-                        <i class="fa-solid fa-triangle-exclamation text-danger"></i> Não Confirmado
+                        <span class="text-danger"><i class="fa-solid fa-triangle-exclamation"></i> Não Confirmado</span>
                         <?php
                         }
                         ?>
