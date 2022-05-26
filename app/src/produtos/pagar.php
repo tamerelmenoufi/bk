520dcc9efcb2ea99161459dc464bdf77feab2079
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-25 22:53:42 -0400

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -201,7 +201,9 @@
                                             $vlopc = 0;
                                             while($v = mysqli_fetch_object($r)){
 
+
                                                 $valores = json_decode($bee->ValorViagem($v->id, $lat, $lng));
+                                                if($valores->deliveryFee > 1){
                                                 if($vlopc >= $valores->deliveryFee) { $vlopc = $valores->deliveryFee; $opc = $v->codigo; }
                                         ?>
                                             <li opc="<?=$v->codigo?>" class="opcLoja list-group-item d-flex justify-content-between align-items-center">
@@ -211,6 +213,7 @@
                                                 </span>
                                             </li>
                                         <?php
+                                                }
                                             }
 
                                         ?>
