968903b48fa24d0d65fb6eba87b765e8110e8bc6
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-31 16:32:17 -0400

    Atualizações

--- lib/vendor/mercado_pago/ObterPagamento.php
+++ lib/vendor/mercado_pago/ObterPagamento.php
@@ -3,12 +3,12 @@
     include('classes.php');
 
     $PIX = new MercadoPago;
-    // 22777629972
-    $retorno = $PIX->ObterPagamento(101770366370);
+
+    $retorno = $PIX->ObterPagamento(22777629972);
 
 
       $dados = json_decode($retorno);
-
+        echo "<pre>";
       print_r($dados);
-
+      echo "</pre>";
 
