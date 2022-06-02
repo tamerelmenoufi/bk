4ead19abe55186340e11a92eb747a1cab7118dec
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-02 09:40:55 -0400

    Atualizações

--- lib/vendor/mercado_pago/webhook.php
+++ lib/vendor/mercado_pago/webhook.php
@@ -1,5 +1,9 @@
 <?php
 
+    include("../../includes.php");
+
+    echo "<h1>{$md5}</h1>";
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
 
@@ -11,6 +15,12 @@
     if($_POST){
         file_put_contents("logs/retorno_".date("YmdHis").".txt",$dadosLog);
 
-        
+        $dados = json_decode($_POST);
+
+        $operadora_id = $dados->id;
+        $forma_pagamento = $dados->payment_method_id;
+        $operadora_situacao = $dados->status;
+        $qrcode = $dados->point_of_interaction->transaction_data->qr_code;
+        $qrcode_img = $dados->point_of_interaction->transaction_data->qr_code_base64;
 
     }
