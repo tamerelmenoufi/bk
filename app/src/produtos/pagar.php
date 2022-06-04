84ef0126f5e7a64f534aa7ae10a5e2b129657c80
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-03 23:46:53 -0400

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -290,7 +290,7 @@
                     <h5 class="card-title">
                         <div class="row">
                             <div class="col">
-                                <img style="padding-radius:5px;" src="src/produtos/captcha.php?l=150&a=45&tf=20&ql=5">
+                                <img style="border-radius:5px;" src="src/produtos/captcha.php?l=150&a=45&tf=20&ql=5">
                             </div>
                             <div class="col">
                                 <input id="captcha" type="text" class="form-control form-control-lg" style="text-align:center; font-weight:bold" />
