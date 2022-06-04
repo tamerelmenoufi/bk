5680c78b4918e4368fd078d95bc409685824bfc2
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-04 00:15:31 -0400

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -266,8 +266,8 @@
                 <div class="card-body">
                     <h5 class="card-title">
                         <div class="row">
-                            <div class="imagemCaptcha" class="col">
-                                <img style="border-radius:5px;" src="src/produtos/captcha.php?l=150&a=45&tf=20&ql=5">
+                            <div class="col">
+                                <img class="imagemCaptcha" style="border-radius:5px;" src="src/produtos/captcha.php?l=150&a=45&tf=20&ql=5">
                             </div>
                             <div class="col">
                                 <input id="captcha" type="text" class="form-control form-control-lg" style="text-align:center; font-weight:bold" />
@@ -396,7 +396,7 @@
                     ql:5
                 },
                 success:function(dados){
-                    var base64 = "data:image/jpeg;base64," + btoa(dados)
+                    var base64 = "data:image/jpeg;base64," + btoa(dados);
                     $(".imagemCaptcha").attr("src",base64);
                 }
             });
