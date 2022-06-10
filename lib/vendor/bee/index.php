331d4d128f407487ae33783d422bc247d89d41cb
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-10 08:58:20 -0400

    Atualizações

---
+++ lib/vendor/bee/index.php
@@ -0,0 +1,22 @@
+<?php
+    include("../../includes.php");
+
+    echo "Na aplicação webhook bee da BK";
+
+    if($_POST['webhook']){
+
+
+
+        // {
+        //     "deliveryId": "88566", // Bee delivery entrega ID
+        //     "externalId": "18238", // empresa integrada pedido ID
+        //     "status": "GOING_TO_ORIGIN",
+        //     "statusDate": "2019-02-01T03:45:27+00:00", // formato Iso 8601
+        //     "desName": "beedelivery",
+        //     "worker": {
+        //         "name": "RAMON SARMENTO",
+        //         "phone": "(84) 123456789"
+        //     }
+        // }
+
+    }
