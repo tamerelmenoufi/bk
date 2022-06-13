3106d46100497ad176b75c60b0d14c073bcf5115
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-13 00:01:03 -0400

    atualizações

--- lib/footer.php
+++ lib/footer.php
@@ -34,6 +34,6 @@
     ></script>
 
     <script src="<?= "{$caminho_vendor}/html2canvas/html2canvas.min.js"; ?>" ></script>
-    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-
+    <!-- <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script> -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     <script src="<?= "{$caminho_vendor}/timeline_vertical/jquery.timeline.min.js"; ?>"></script>
