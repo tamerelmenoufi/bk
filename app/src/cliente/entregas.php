f47a02280f798930cb6fbdcd6349e296694c139a
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-13 08:32:41 -0400

    atualizações

--- app/src/cliente/entregas.php
+++ app/src/cliente/entregas.php
@@ -53,7 +53,7 @@
         </div>
 
         <div style="position:absolute; left:30px; top:10px; right:5px; border-radius:5px; background-color:#ccc; padding:5px;">
-            <div style="position:absolute; left:-10px; top:10px; font-size:20px; color:#ccc;">
+            <div style="position:absolute; left:-7px; top:0px; font-size:25px; color:#ccc;">
                 <i class="fa-solid fa-caret-left"></i>
             </div>
             Informações da entrega
