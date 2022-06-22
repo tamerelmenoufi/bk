034804214ca693d76c307e5e0f822032de3189f1
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-22 09:59:55 -0400

    Atualizações

--- app/src/produtos/observacoes.php
+++ app/src/produtos/observacoes.php
@@ -3,29 +3,33 @@
 ?>
 <div class="col">
     <div class="col">
-        <div>
+
+        <div class="mb-3">
             <h4>Incluir Observações</h4>
             <textarea class="form-control" id="observacoes"></textarea>
         </div>
 
-        <div class="card">
-            <h5 class="card-header">Remover Itens do produto</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="mb-3">
+            <div class="card">
+                <h5 class="card-header">Remover Itens do produto</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         </div>
 
-        <div class="card">
-            <h5 class="card-header">Adicionar Itens ao produto</h5>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="mb-3">
+            <div class="card">
+                <h5 class="card-header">Adicionar Itens ao produto</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         </div>
-
 
     </div>
 </div>
