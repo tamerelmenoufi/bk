9376f1759986867d2c8758c5adf57127d72edadd
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-22 10:22:43 -0400

    Atualizações

--- app/src/produtos/observacoes.php
+++ app/src/produtos/observacoes.php
@@ -47,15 +47,11 @@
         <div class="mb-3">
             <div class="card">
                 <h5 class="card-header">Adicionar Itens ao produto</h5>
-                <div class="card-body">
-
-                    <ul class="list-group">
-                        <li class="list-group-item">Cras justo odio</li>
-                        <li class="list-group-item">Dapibus ac facilisis in</li>
-                        <li class="list-group-item">Vestibulum at eros</li>
-                    </ul>
-
-                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
             </div>
         </div>
 
