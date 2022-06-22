d843fa7bede769b786ea30f06ec01eb08c50d800
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-22 08:44:30 -0400

    Atualizações

--- app/componentes/ms_lojas.php
+++ app/componentes/ms_lojas.php
@@ -3,24 +3,27 @@
 ?>
 
 <style>
-
+    .PainelLojas{
+        padding:10px;
+    }
 </style>
 
-
-<div class="row">
-    <div class='col'>
-        <?php
-            $query = "select * from lojas where situacao = '1' order by nome";
-            $result = mysqli_query($con, $query);
-            while($d = mysqli_fetch_object($result)){
-        ?>
-        <button class="btn btn-primary btn-block mb-2" loja="<?=$d->codigo?>"><?=$d->nome?></button>
-        <?php
-            }
-        ?>
+<div class="PainelLojas">
+    <div class="row">
+        <div class='col'>
+            <h4>Selecione a Loja se sua preferência</h4>
+            <?php
+                $query = "select * from lojas where situacao = '1' order by nome";
+                $result = mysqli_query($con, $query);
+                while($d = mysqli_fetch_object($result)){
+            ?>
+            <button class="btn btn-primary btn-block mb-2" loja="<?=$d->codigo?>"><?=$d->nome?></button>
+            <?php
+                }
+            ?>
+        </div>
     </div>
 </div>
-
 <script>
     $(function(){
 
