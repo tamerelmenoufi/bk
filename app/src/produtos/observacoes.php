58d035f9e5bd2d737f552a8cb4893f8c949db497
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-22 09:58:20 -0400

    Atualizações

--- app/src/produtos/observacoes.php
+++ app/src/produtos/observacoes.php
@@ -3,8 +3,30 @@
 ?>
 <div class="col">
     <div class="col">
-        <h4>Incluir Observações</h4>
-        <textarea class="form-control" id="observacoes"></textarea>
+        <div>
+            <h4>Incluir Observações</h4>
+            <textarea class="form-control" id="observacoes"></textarea>
+        </div>
+
+        <div class="card">
+            <h5 class="card-header">Remover Itens do produto</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <h5 class="card-header">Adicionar Itens ao produto</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+
     </div>
 </div>
 
