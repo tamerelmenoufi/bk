aea11b57736ab377024de3f65a436aa974a0d144
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-22 10:27:51 -0400

    Atualizações

--- app/src/produtos/observacoes.php
+++ app/src/produtos/observacoes.php
@@ -32,9 +32,26 @@
             <div class="card">
                 <h5 class="card-header"><i class="fa-solid fa-eraser"></i> <small> Remover Itens do produto</small></h5>
                 <ul class="list-group">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
+
+                    <li class="list-group-item">
+
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+
+                    </li>
+
                 </ul>
             </div>
         </div>
