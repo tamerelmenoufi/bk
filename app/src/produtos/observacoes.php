72082bd777a5e762bdd6cb3dfc8abc9211ed11d1
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-23 09:46:02 -0400

    Atualizações

--- app/src/produtos/observacoes.php
+++ app/src/produtos/observacoes.php
@@ -91,7 +91,8 @@
                         $result = mysqli_query($con, $query);
                         while($d = mysqli_fetch_object($result)){
                     ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <!-- d-flex justify-content-between align-items-center -->
+                    <li class="list-group-item">
                         <div class="row">
                             <div class="col">
                                 <div class="form-check">
