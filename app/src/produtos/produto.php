0a0b62b65cea5363a3a13eb466c67f97f38e9d89
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-23 09:12:31 -0400

    Atualizações

--- app/src/produtos/produto.php
+++ app/src/produtos/produto.php
@@ -186,7 +186,7 @@
                                     border-radius:100%;
                                     background-image:url(<?=$caminho_sis?>/painel/produtos/icon/<?= $p->icon ?>);
                                     background-size:cover;
-                                    background-position:center center;
+                                    /*background-position:center center;*/
                                     opacity:0.3;
                                     filter: blur(5px);
                                     text-align:center;
