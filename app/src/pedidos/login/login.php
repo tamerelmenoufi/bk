cf81bdf031f0a7b84748d09bd7e0f52ec9152c9b
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-15 09:49:30 -0400

    Atualizações

--- app/src/pedidos/login/login.php
+++ app/src/pedidos/login/login.php
@@ -1,19 +1,22 @@
 <h3>Tela de login</h3>
 <p>Dados de acesso do meu login da loja</p>
-<button class="btn btn-success">Logar</button>
+<button logar class="btn btn-success">Logar</button>
 
 
 <script>
     $(function(){
-        $.ajax({
-            url:"componentes/ms_popup_100.php",
-            type:"POST",
-            data:{
-                local:"src/pedidos/home/index.php",
-            },
-            success:function(dados){
-                $(".ms_corpo").append(dados);
-            }
+        $("button[logar]").click(function(){
+            $.ajax({
+                url:"componentes/ms_popup_100.php",
+                type:"POST",
+                data:{
+                    local:"src/pedidos/home/index.php",
+                },
+                success:function(dados){
+                    $(".ms_corpo").append(dados);
+                }
+            });
         });
+
     })
 </script>
