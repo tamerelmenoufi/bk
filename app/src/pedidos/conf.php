b87072d150a3e5a5e59025c83e75917db7d8dc51
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-15 11:34:13 -0400

    Atualizações

--- app/src/pedidos/conf.php
+++ app/src/pedidos/conf.php
@@ -1,4 +1,4 @@
 <?php
-    include("../../lib/includes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
     if($_SESSION['PedidosUsuario']) $Uconf = $_SESSION['PedidosUsuario'];
     else $Uconf = [];
