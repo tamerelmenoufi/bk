dc80e733856f2d407fb10e0d2246fe69a133d3e4
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-15 08:52:20 -0400

    Atualizações

--- app/src/cliente/entregas.php
+++ app/src/cliente/entregas.php
@@ -22,7 +22,7 @@
                         '.$obj['hora'].'
                     </div>
 
-                    <div style="position:absolute; height:23px; width:23px; margin:0; padding:0; left:-13px; top:10px; font-size:23px; background-color:red; border-radius:100%;">
+                    <div style="position:absolute; height:23px; width:23px; margin:0; padding:0; left:-13px; top:10px; font-size:23px; background-color:#fff; border-radius:100%;">
                         <i class="fa-solid fa-clock" style="position:absolute; margin:0; padding:0"></i>
                     </div>
 
