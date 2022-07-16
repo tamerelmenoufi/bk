fb7ac69fefa3cca56c7fb6a33e84a3d9fc20689b
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-15 21:39:26 -0400

    Atualizações

--- app/src/pedidos/ListaStatus/index.php
+++ app/src/pedidos/ListaStatus/index.php
@@ -3,8 +3,14 @@
 
     echo date("d/m/Y H:i:s");
 ?>
-
-<audio autoplay="autoplay" controls="controls">
+<style>
+    .som{
+        position:fixed;
+        left:-1000px;
+        top:-1000px;
+    }
+</style>
+<audio autoplay="autoplay" controls="controls" class="som">
     <source src="src/pedidos/ListaStatus/som/pedido.mp3" type="audio/mp3" />
 </audio>
 
