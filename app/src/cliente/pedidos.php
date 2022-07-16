b5c5d03aac915dff37a38ba9e096a69d82a84356
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-16 17:08:06 -0400

    Atualizações

--- app/src/cliente/pedidos.php
+++ app/src/cliente/pedidos.php
@@ -21,7 +21,7 @@
     <div class="row">
         <div class="col">
 <?php
-    $query = "select * from vendas where cliente = '{$_SESSION['AppCliente']}' and deletado != '1' and operadora_situacao = 'approved' /*and data_finalizacao > 0*/ order by data_pedido desc";
+    $query = "select * from vendas where cliente = '{$_SESSION['AppCliente']}' and deletado != '1' and (operadora_situacao = 'approved' or forma_pagamento = 'pix') /*and data_finalizacao > 0*/ order by data_pedido desc";
     $result = mysqli_query($con, $query);
     $n = mysqli_num_rows($result);
     while($d = mysqli_fetch_object($result)){
