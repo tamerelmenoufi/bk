3b83e470ba06115edc8f1dff9cd0308c489c2993
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-28 08:24:42 -0400

    Atualizações

--- painel/combos/imagem.php
+++ painel/combos/imagem.php
@@ -49,7 +49,7 @@ function imagem($source)
         position: relative;
         width: 100%;
         height: 250px;
-        background-color: #f2e5d4;
+        background-color: #aa3a15;
         text-align: center;
     }
 
