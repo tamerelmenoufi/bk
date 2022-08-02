d060e5ebff2647dd13243428265594c898ba3ced
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-02 16:17:23 -0400

    Atualizações

--- app/src/cliente/alert_pedido.php
+++ app/src/cliente/alert_pedido.php
@@ -1,8 +1,20 @@
 <?php
     include("../../../lib/includes.php");
-
-    if(date("s") >= 10 and date("s") <= 15){
-        echo "TTT";
+?>
+<style>
+    .Alerta{
+        position:absolute;
+        width:auto;
+        height:30px;
+        margin:15px;
+        border:solid 1px blue;
+        border-radius:7px;
+    }
+</style>
+<?php
+    $s = date("s");
+    if($s >= 10 and $s <= 15){
+        echo "<div class='Alerta'>{$s}</div>";
     }
 
 ?>
