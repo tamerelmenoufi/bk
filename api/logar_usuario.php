c8e1705688fd6faa1432e02da942a857e642fbc8
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-19 11:14:50 -0400

    Atualizações

--- api/logar_usuario.php
+++ api/logar_usuario.php
@@ -5,7 +5,7 @@ include("../lib/includes.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$query = "SELECT * FROM usuarios cpf = '{$_POST['cpf']}' and senha = '".md5($_POST['senha'])."'";
+$query = "SELECT * FROM usuarios where cpf = '{$_POST['cpf']}' and senha = '".md5($_POST['senha'])."'";
 $result = mysqli_query($con, $query);
 $dados = [];
 if(mysqli_num_rows($result)){
