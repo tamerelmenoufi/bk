998c29f3a2691b521e9b8b37ef72a3ec7f921b3a
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-19 09:38:39 -0400

    Atualizações

--- api/lista_lojas.php
+++ api/lista_lojas.php
@@ -2,7 +2,7 @@
 
 include("../lib/includes.php");
 
-$query = "select * from lojas where situacao = '1' order by loja";
+$query = "select codigo, nome from lojas where situacao = '1' order by nome";
 $result = mysqli_query($con, $query);
 $dados = [];
 while($d = mysqli_fetch_object($result)){
