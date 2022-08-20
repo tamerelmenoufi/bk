a9ad18ff92f3e5a73a97c882db34c58a778fa8ba
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-20 08:46:12 -0400

    Atualizações

--- api/lista_pedidos.php
+++ api/lista_pedidos.php
@@ -2,11 +2,12 @@
 
 include("../lib/includes.php");
 
-$query = "SELECT * FROM `vendas`";
+$query = "SELECT a.codigo, c.nome FROM vendas a left join clientes c on a.cliente = c.codigo";
 $result = mysqli_query($con, $query);
 $dados = [];
 while($d = mysqli_fetch_object($result)){
-    $dados[] = $d;
+    $pedido = str_pad($d->codigo , 5 , '0' , STR_PAD_LEFT);
+    $dados[] = ['codigo'=> $d->codigo, 'pedido' => $pedido, 'cliente' => $d->nome];
 }
 
 echo json_encode($dados);
