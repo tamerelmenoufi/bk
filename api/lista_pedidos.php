395e3f0bceed02f76a1a6595f680074580d98230
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-20 08:50:02 -0400

    Atualizações

--- api/lista_pedidos.php
+++ api/lista_pedidos.php
@@ -6,7 +6,7 @@ $query = "SELECT a.codigo, c.nome FROM vendas a left join clientes c on a.client
 $result = mysqli_query($con, $query);
 $dados = [];
 while($d = mysqli_fetch_object($result)){
-    $pedido = str_pad($d->codigo , 5 , '0' , STR_PAD_LEFT);
+    $pedido = '#'.str_pad($d->codigo , 5 , '0' , STR_PAD_LEFT);
     $dados[] = ['codigo'=> $d->codigo, 'pedido' => $pedido, 'cliente' => $d->nome];
 }
 
