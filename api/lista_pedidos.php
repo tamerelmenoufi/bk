eff8e08a0fffa587dbea902688bad2dd20824642
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-21 01:34:32 -0400

    Atualizações

--- api/lista_pedidos.php
+++ api/lista_pedidos.php
@@ -5,9 +5,9 @@ include("../lib/includes.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$query = "SELECT a.codigo, c.nome FROM vendas a left join clientes c on a.cliente = c.codigo where situacao = 'p' and a.deletado != '1' and loja = '{$_POST['loja']}'";
+$query = "SELECT a.codigo, c.nome FROM vendas a left join clientes c on a.cliente = c.codigo where a.situacao = 'p' and a.deletado != '1' and a.loja = '{$_POST['loja']}'";
 $result = mysqli_query($con, $query);
-$dados = [$query];
+$dados = [];
 while($d = mysqli_fetch_object($result)){
     $pedido = '#'.str_pad($d->codigo , 5 , '0' , STR_PAD_LEFT);
     $dados[] = ['codigo'=> $d->codigo, 'pedido' => $pedido, 'cliente' => $d->nome];
