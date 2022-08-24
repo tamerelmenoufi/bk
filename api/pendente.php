c963668adf3d241625a299a46217265f93e7d4fa
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 10:46:31 -0400

    Atualizações

--- api/pendente.php
+++ api/pendente.php
@@ -5,9 +5,13 @@ include("../lib/includes.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$query = "select * from vendas where data_pedido LIKE '%".date("Y-m-d")."%' and deletado != '1' and situacao = 'n'";
+$query = "select a.*, (select count(*) from vendas_produtos where venda = a.codigo) as qt from vendas a where a.data_pedido LIKE '%".date("Y-m-d")."%' and a.deletado != '1' and a.situacao = 'n'";
 $result = mysqli_query($con, $query);
+$p = 0;
+while($d = mysqli_fetch_object($result)){
+    if($d->qt > 0) $p++;
+}
 
-$dados = ['status' => ((mysqli_num_rows($result))?true:false), 'loja' => $_POST['loja']];
+$dados = ['status' => ((mysqli_num_rows($result) and $p > 0)?true:false), 'loja' => $_POST['loja']];
 
 echo json_encode($dados);
