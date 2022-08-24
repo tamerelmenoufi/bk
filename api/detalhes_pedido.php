191bdba1d7934a6e2e3db074e5e2d63cc42c1830
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 11:09:54 -0400

    Atualizações

--- api/detalhes_pedido.php
+++ api/detalhes_pedido.php
@@ -5,7 +5,7 @@ include("../lib/includes.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$query = "SELECT a.*, c.categoria as nome_categoria FROM vendas_produtos a left join produtos b on a.produto = b.codigo left join categorias c on b.categoria = c.codigo where a.venda = '{$_POST['pedido']}'";
+$query = "SELECT a.*, c.categoria as nome_categoria FROM vendas_produtos a left join produtos b on a.produto = b.codigo left join categorias c on b.categoria = c.codigo where a.venda = '{$_POST['pedido']}' and a.deletado != '1'";
 $result = mysqli_query($con, $query);
 $dados = [];
 while($d = mysqli_fetch_object($result)){
