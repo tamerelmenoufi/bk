3f861361443e03db4a2bbe503edcaf578fd520cc
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 10:14:02 -0400

    Atualizações

--- api/pendente.php
+++ api/pendente.php
@@ -5,7 +5,7 @@ include("../lib/includes.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$query = "select * from vendas where data_pedido = '%".date("Y-m-d")."%' and deletado != '1' and situacao = 'n'";
+$query = "select * from vendas where data_pedido LIKE '%".date("Y-m-d")."%' and deletado != '1' and situacao = 'n'";
 $result = mysqli_query($con, $query);
 
 $dados = ['status' => ((mysqli_num_rows($result))?true:false), 'loja' => $_POST['loja']];
