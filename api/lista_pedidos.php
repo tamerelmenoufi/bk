ba5e9b831cfe9a119b435a6ad59b975bb850b46b
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 10:48:11 -0400

    Atualizações

--- api/lista_pedidos.php
+++ api/lista_pedidos.php
@@ -16,7 +16,8 @@ $query = "SELECT
 $query = "SELECT
                 a.codigo,
                 a.situacao,
-                c.nome
+                c.nome,
+                (select count(*) from vendas_produtos where venda = a.codigo) as qt
                 FROM vendas a
                 left join clientes c on a.cliente = c.codigo
         where a.data_pedido LIKE '%".date("Y-m-d")."%' and a.deletado != '1' and a.situacao = 'n'";
@@ -24,8 +25,10 @@ $query = "SELECT
 $result = mysqli_query($con, $query);
 $dados = [];
 while($d = mysqli_fetch_object($result)){
-    $pedido = '#'.str_pad($d->codigo , 5 , '0' , STR_PAD_LEFT);
-    $dados[] = ['codigo'=> $d->codigo, 'pedido' => $pedido, 'cliente' => $d->nome, 'situacao' => $d->situacao];
+    if($d->qt > 0){
+        $pedido = '#'.str_pad($d->codigo , 5 , '0' , STR_PAD_LEFT);
+        $dados[] = ['codigo'=> $d->codigo, 'pedido' => $pedido, 'cliente' => $d->nome, 'situacao' => $d->situacao];
+    }
 }
 
 echo json_encode($dados);
