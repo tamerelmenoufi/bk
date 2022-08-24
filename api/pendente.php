21c1d1eb87b41c884495e82c54450fdbfd2c2757
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 09:08:26 -0400

    Atualizações

---
+++ api/pendente.php
@@ -0,0 +1,10 @@
+<?php
+
+include("../lib/includes.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$dados[] = ['status' => true];
+
+echo json_encode($dados);
