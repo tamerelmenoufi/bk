ce1016daf094363b7a618007da9081f35f3b189a
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-24 23:54:55 -0400

    Atualizações

---
+++ api/mudar_status.php
@@ -0,0 +1,13 @@
+<?php
+
+include("../lib/includes.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+$_POST = json_decode(file_get_contents('php://input'), true);
+$dados = [];
+if($_POST['status'] and $_POST['pedido']){
+    $query = "UPDATE vendas set status = 'i' where codigo = '{$_POST['pedido']}'";
+    $result = mysqli_query($con, $query);
+    $dados = ['status' => true];
+}
+echo json_encode($dados);
