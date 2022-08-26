f5add06b98626d6ce5fe805acb57eed1de7e0deb
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-25 22:46:42 -0400

    Atualizações

--- api/detalhes_pedido.php
+++ api/detalhes_pedido.php
@@ -42,6 +42,8 @@ while($d = mysqli_fetch_object($result)){
         $status .= "<p>Entrega realizada <small>".($d->ARRIVED_AT_DESTINATION)."</small></p>";
     }
 
+    $staus = '<div style="position:absolute; bottom:0; width:100%">'.$status.'</div>';
+
     $d->produto_descricao .= $status;
 
     $dados[] = $d;
