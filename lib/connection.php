415d9cbe157358b35203a2421cb2ff2d833adfa6
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-14 10:08:18 -0400

    Atualizações

--- lib/connection.php
+++ lib/connection.php
@@ -5,10 +5,10 @@ if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '192.168.
     define('DB_PASSWORD', '');
     define('DB_DATABASE', 'sis_bk');
 } else {
-    define('DB_HOST', 'bk.mohatron.com');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'SenhaDoBanco');
-    define('DB_DATABASE', 'bk');
+    define('DB_HOST', $cBk['banco']['HOST']);
+    define('DB_USERNAME', $cBk['banco']['USERNAME']);
+    define('DB_PASSWORD', $cBk['banco']['PASSWORD']);
+    define('DB_DATABASE', $cBk['banco']['DATABASE']);
 }
 
 $con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
