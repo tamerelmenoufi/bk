8309ec34ec1233382175b5ce8f2a12bec51bf2a3
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-14 15:46:45 -0400

    Atualizações

--- app/src/cliente/alert_pedido.php
+++ app/src/cliente/alert_pedido.php
@@ -1,16 +1,34 @@
 <?php
     include("../../../lib/includes.php");
+
+    $tempo = date("Y-m-d H:i:s", mktime((date("H") - 12), date("i"), date("s"), date("m"), date("d"), date("Y")));
+
+
+
+    $query = "select * vendas where
+                                    data_finalizacao <= '{$tempo}' and
+                                    forma_pagamento = 'pix' and
+                                    operadora_situacao = 'pending' and
+                                    deletado != '1'
+                                    ";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+    if($d->codigo){
+
 ?>
 <style>
-    .Alerta{
-        position:absolute;
+    .alerta{
+        position:fixed;
         width:auto;
         height:30px;
-        margin:15px;
-        border:solid 1px blue;
+        left:10px;
+        top:10;
         border-radius:7px;
+        border:solid 1px #333;
+        background-color:#ccc;
+        z-index:10;
     }
 </style>
 <?php
-    // $query = "select * from vendas where "
+    }
 ?>
