2d07e08861f40e5eeb56b23f9f88788c9afcef4f
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-15 09:26:56 -0400

    Atualizações

--- app/src/cliente/pedidos.php
+++ app/src/cliente/pedidos.php
@@ -43,7 +43,7 @@
                     <p class="card-text">
                         Pedido: <?=str_pad($d->codigo, 6, "0", STR_PAD_LEFT)?><br>
                         Valor: R$ <?=$d->total?><br>
-                        Data do Pedido: <?=$d->data_pedido?><br>
+                        Data do Pedido: <?=formata_datahora($d->data_pedido)?><br>
                         <!-- Data Nova: <?=$d->dtNova?><br> -->
                         Forma de Pagamento: <?=strtoupper($d->forma_pagamento)?><br>
                         situação do Pagamento: <?=$d->operadora_situacao?>
