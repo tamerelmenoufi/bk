d3067357d0be51f15e9f32821d7f344f92587c44
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-19 16:35:52 -0400

    Atualizações

---
+++ painel/lojas.php
@@ -0,0 +1,14 @@
+<?php
+include("../lib/includes.php");
+
+$query = "select * from lojas";
+$result = mysqli_query($con, $query);
+$dados = [];
+while($d = mysqli_fetch_object($result)){
+    $dados[$d->codigo] = [
+        "loja" => $d->loja,
+        "situacao" => $d->situacao
+    ];
+}
+
+echo json_encode($dados);
