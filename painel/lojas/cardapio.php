87c5d4a9c5112e460c688d690aa1213adeb16579
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-20 09:05:50 -0400

    Atualizações

--- painel/lojas/cardapio.php
+++ painel/lojas/cardapio.php
@@ -1,6 +1,7 @@
 <?php
     include("../../lib/includes.php");
     echo $_GET['cod'];
+    echo "<br><br>";
 
     $query = "select a.*, b.categoria from produtos a left join categorias b on a.categoria = b.codigo where a.deletado != '1' and b.deletado != '1' and a.situacao = '1' and b.situacao = '1' order by b.ordem asc, a.produto asc";
     $result = mysqli_query($con, $query);
@@ -8,7 +9,14 @@
     while($d = mysqli_fetch_object($result)){
         if($categoria != $d->categoria){
             $categoria = $d->categoria;
-            echo "<h4>{$categoria}</h4>";
+?>
+    <div class="row">
+        <div class="col-md-10"><?=$d->produto?></div>
+        <div class="col-md-2">
+            Ação
+        </div>
+    </div>
+<?php
         }
         echo "{$d->produto}<br>";
     }
