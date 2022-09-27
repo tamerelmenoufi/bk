75f3cc6bab5963b0c87675882f3a02cd239ed996
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-27 10:06:50 -0400

    Atualizações

--- site/src/produtos/lista.php
+++ site/src/produtos/lista.php
@@ -28,17 +28,17 @@
                 $result = mysqli_query($con, $query);
                 while($d = mysqli_fetch_object($result)){
             ?>
-            <div class="card m-3">
+            <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-4">
-                    <img src="<?=$caminho_sis.'/painel/produtos/icon/'.$d->icon?>" class="img-fluid rounded-start" >
+                        <img src="<?=$caminho_sis.'/painel/produtos/icon/'.$d->icon?>" class="img-fluid rounded-start" >
                     </div>
                     <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title"><?=$d->produto?></h5>
-                        <p class="card-text"><?=$d->descricao?></p>
-                        <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
-                    </div>
+                        <div class="card-body">
+                            <h5 class="card-title"><?=$d->produto?></h5>
+                            <p class="card-text"><?=$d->descricao?></p>
+                            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                        </div>
                     </div>
                 </div>
             </div>
