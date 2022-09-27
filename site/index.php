aa0d276998fa1ca9f6b36730a241e3dc27d7ad54
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-26 21:50:16 -0400

    Atualizações

--- site/index.php
+++ site/index.php
@@ -650,8 +650,10 @@
             $q = "select * from produtos where codigo in ({$des})";
             $r = mysqli_query($con, $q);
             $im = [];
+            $x=0;
             while($p = mysqli_fetch_object($r)){
-              $im[] = '<img src="'.$caminho_sis.'/painel/produtos/icon/'.$p->icon.'" style="width:50%; position:absolute; left:-25%; border:solid 1px red">';
+              $im[] = '<img src="'.$caminho_sis.'/painel/produtos/icon/'.$p->icon.'" style="width:50%; position:absolute; left:-'.$i.'%; border:solid 1px red">';
+              $i = ($i+25);
             }
             if($im){ $icon = implode("",$im); }else{$icon = false;}
           }else{
