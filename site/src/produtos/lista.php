915a2b54c21df518c744a1868156c63d0e2485bb
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-26 23:04:01 -0400

    Atualizações

--- site/src/produtos/lista.php
+++ site/src/produtos/lista.php
@@ -1,22 +1,32 @@
-
+<?php
+    include("../../../lib/includes.php");
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-6 offset-md-3" style="background-color:#fff; margin-top:50px;">
+        <?php
+            $query = "select * from produtos where 1";
+            $result = mysqli_query($con, $query);
+            while($d = mysqli_fetch_object($result)){
+        ?>
         <div class="card m-3">
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
+                <img src="<?=$caminho_sis.'/painel/combos/icon/'.$d->icon?>" class="img-fluid rounded-start" >
                 </div>
                 <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title"><?=$d->produto?></h5>
+                    <p class="card-text"><?=$d->descricao?></p>
+                    <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
                 </div>
                 </div>
             </div>
             </div>
         </div>
+        <?php
+            }
+        ?>
     </div>
 </div>
 
