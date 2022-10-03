38fc04ca39b96f108ed8e572bc6e40018eda7b6d
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-03 16:51:59 -0400

    Atualizações

--- site/index.php
+++ site/index.php
@@ -1616,9 +1616,6 @@
 
   <div id="preloader"></div>
 
-
-  <h1 style="font-family:Flame-Regular;">Teste da nova fonte</h1>
-
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/aos/aos.js"></script>
