f83dd7bb39cdad5d3bee98d2cb076ee244ac4a25
livia128924 <livia128924@gmail.com>
2022-10-05 09:44:56 -0300

    atualização

--- site/index.php
+++ site/index.php
@@ -146,6 +146,7 @@
   </header><!-- End Header -->
 
   
+<divc class="d-black d-md-none"> 
 
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
@@ -173,6 +174,40 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+</div>
+
+</div>
+
+<divc class="d-none d-sm-block"> 
+
+<div id="carouselExampleControls1" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active ">
+    <a href="https://app.bkmanaus.com.br" target="_blank" class="btn-get-started scrollto"> 
+      <img src="assets/img/banners/bks.jpg" class="d-block w-100" alt="...">
+      </a>
+    </div>
+   
+
+    <div class="carousel-item">
+    <a href="https://app.bkmanaus.com.br" target="_blank" class="btn-get-started scrollto"> 
+    <img src="assets/img/banners/banner3.jpg" class="d-block w-100" alt="...">
+      </a>
+    </div>
+   
+
+
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
 </div>
 
 
