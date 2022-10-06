5dad9fec8e096330f691969589592aaf822ddcbe
livia128924 <livia128924@gmail.com>
2022-10-06 13:16:54 -0300

    atualização

--- site/src/produtos/lista.php
+++ site/src/produtos/lista.php
@@ -32,19 +32,16 @@
     <i class="bi bi-x"></i>
 </span>
 
-<div class="titulo_categoria">
-    <div class="container">
-        <div class="row" >
-            <div class="" style="background-color:#fff; ">
-                <center><h1><?=$_POST['descricao']?></h1></center>
-            </div>
-        </div>
-    </div>
-</div>
 
 <div class="container">
     <div class="row">
         <div class="" style="background-color:#fff; padding-top:60px;">
+
+        <div class="" style="background-color:#fff; ">
+                <center><h1><?=$_POST['descricao']?></h1></center>
+            </div>
+
+
             <?php
                 $query = "select * from produtos where categoria = '{$_POST['codigo']}' and situacao = '1' and deletado != '1'";
                 $result = mysqli_query($con, $query);
@@ -52,7 +49,7 @@
             ?>
             <div class="card mb-3">
                 <div class="row g-0">
-                    <div class="col-md-4" style="background-color:rgb(170,58,21,1); display: flex; align-items: center; justify-content: center;">
+                    <div class="col-md-4" style="background-color:rgb(255, 170, 0); display: flex; align-items: center; justify-content: center;">
                         <img src="<?=$caminho_sis."/painel/".(($_POST['codigo'] == 8)?'combos':'produtos')."/icon/".$d->icon?>" class="img-fluid rounded-start" >
                     </div>
                     <div class="col-md-8">
