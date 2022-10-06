3472288f8951834c3a447ba3600eaa2e9cfd9f46
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-05 23:57:08 -0400

    Atualizações

--- painel/combos/form.php
+++ painel/combos/form.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (file_put_contents("icon/" . md5($codigo) . ".png", $icon)) {
 
                 $img = imagecreatefrompng("icon/" . md5($codigo) . ".png");
-                $white = imagecolorallocatealpha($img, 255, 255, 255, 10);
+                $white = imagecolorallocatealpha($img, 255, 255, 255, 0);
                 imagecolortransparent($img, $white);
                 imagepng($img,"icon/" . md5($codigo) . ".png");
 
