972e1a881b2c82235f8e1b1df594e8090d2bb9d5
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-07 22:10:35 -0400

    Atualizações

--- lib/verificar_itens.php
+++ lib/verificar_itens.php
@@ -5,32 +5,41 @@
         global $con;
         $query = "select * from produtos where codigo in (".implode(",",$cod).")";
         $result = mysqli_query($con, $query);
-        $Itens = [];
+
         $status = [];
+
         while($d = mysqli_fetch_object($result)){
+
+            $Itens = [];
+
+
             $itens = json_decode($d->itens);
             foreach($itens as $i => $val){
                 $Itens[] = $val->produto;
             }
-        }
 
-        if($Itens){
 
-            $query = "select * from itens where codigo in (".implode(",",$Itens).")";
-            $result = mysqli_query($con, $query);
+            if($Itens){
 
-            while($d = mysqli_fetch_object($result)){
-                $lojas = json_decode($d->lojas);
+                $query1 = "select * from itens where codigo in (".implode(",",$Itens).")";
+                $result1 = mysqli_query($con, $query1);
 
-                // print_r($lojas);
-                // echo "<hr>";
+                while($d1 = mysqli_fetch_object($result1)){
+                    $lojas = json_decode($d1->lojas);
 
-                foreach($lojas as $i => $val){
-                    $status[$i][$d->codigo] = $val->situacao;
+                    // print_r($lojas);
+                    // echo "<hr>";
+
+                    foreach($lojas as $i => $val){
+                        $status[$d->codigo][$i][$d->codigo] = $val->situacao;
+                    }
                 }
+
             }
 
         }
+
+
 
         if($status) echo "<pre>"; print_r($status); echo "</pre>";
 
