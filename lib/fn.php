efd75227a5e591dabd549ce8a112b82a13d02086
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-08 10:08:30 -0400

    Atualizações

--- lib/fn.php
+++ lib/fn.php
@@ -109,3 +109,42 @@ function VerificarVendaApp(){
 
 ////////////////////////////////////////////////////////////////////////////
 
+function VerificarItens($cod){
+    global $con;
+    $query = "select * from produtos where codigo in (".implode(",",$cod).")";
+    $result = mysqli_query($con, $query);
+
+    $status = [];
+
+    while($d = mysqli_fetch_object($result)){
+
+        $Itens = [];
+
+        $itens = json_decode($d->itens);
+        foreach($itens as $i => $val){
+            $Itens[] = $val->produto;
+        }
+
+        if($Itens){
+
+            $query1 = "select * from itens where codigo in (".implode(",",$Itens).")";
+            $result1 = mysqli_query($con, $query1);
+
+            while($d1 = mysqli_fetch_object($result1)){
+                $lojas = json_decode($d1->lojas);
+
+                foreach($lojas as $i => $val){
+                    //Dentro do Array status[loja][produto][item] = situação
+                    $status[$i][$d->codigo][$d1->codigo] = $val->situacao;
+                }
+            }
+
+        }
+
+    }
+
+    return $status;
+
+    // if($status) echo "<pre>"; print_r($status); echo "</pre>";
+
+}
--- lib/verificar_itens.php
+++ lib/verificar_itens.php
@@ -1,7 +1,7 @@
 <?php
     include("includes.php");
 
-    function VerificarItens($cod){
+    function VerificarProdutos($cod){
         global $con;
         $query = "select * from produtos where codigo in (".implode(",",$cod).")";
         $result = mysqli_query($con, $query);
@@ -10,27 +10,11 @@
 
         while($d = mysqli_fetch_object($result)){
 
-            $Itens = [];
+            $lojas = [];
 
-            $itens = json_decode($d->itens);
-            foreach($itens as $i => $val){
-                $Itens[] = $val->produto;
-            }
-
-            if($Itens){
-
-                $query1 = "select * from itens where codigo in (".implode(",",$Itens).")";
-                $result1 = mysqli_query($con, $query1);
-
-                while($d1 = mysqli_fetch_object($result1)){
-                    $lojas = json_decode($d1->lojas);
-
-                    foreach($lojas as $i => $val){
-                        //Dentro do Array status[loja][produto][item] = situação
-                        $status[$i][$d->codigo][$d1->codigo] = $val->situacao;
-                    }
-                }
-
+            $lojas = json_decode($d->lojas);
+            foreach($lojas as $i => $val){
+                $status[$i][$d->codigo] = $val->situacao;
             }
 
         }
@@ -43,4 +27,4 @@
 
 // print_r($_GET['cod']);
 
-    if($_GET['cod']) VerificarItens($_GET['cod']);
+    if($_GET['cod']) VerificarProdutos($_GET['cod']);
