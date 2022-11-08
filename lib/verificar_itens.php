a4ec49fff210becb8319235e161a4877a28fe38c
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-07 21:32:27 -0400

    Atualizações

--- lib/verificar_itens.php
+++ lib/verificar_itens.php
@@ -1,21 +1,34 @@
 <?php
     include("includes.php");
 
-    function VerificarItens($cod){
+    function VerificarItens($cod, $loja = 10){
         global $con;
-        $query = "select * from produtos where codigo = '{$cod}'";
+        $query = "select * from produtos where codigo in (".implode($cod).")";
         $result = mysqli_query($con, $query);
+        $Itens = [];
+        while($d = mysqli_fetch_object($result)){
+            $itens = json_decode($d->itens);
+            foreach($itens as $i => $val){
+                $Itens[] = $val->produto;
+            }
+        }
 
-        $d = mysqli_fetch_object($result);
+        if($Itens){
 
-        $itens = json_decode($d->itens);
+            $query = "select * from itens where codigo in (".implode($Itens).")";
+            $result = mysqli_query($con, $query);
+            $status = [];
+            while($d = mysqli_fetch_object($result)){
+                $lojas = json_decode($d->lojas);
 
-        print_r($itens);
+                print_r($lojas);
+                echo "<br>";
 
-        echo "<hr>";
+                // foreach($itens as $i => $val){
+                //     $Itens[] = $val->produto;
+                // }
+            }
 
-        foreach($itens as $i => $val){
-            echo $val->produto."<br>";
         }
 
     }
