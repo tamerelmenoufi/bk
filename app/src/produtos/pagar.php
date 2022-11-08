8eca81c0f58b356f3754e13377b13dd52f989aa1
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-08 11:37:52 -0400

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -443,7 +443,7 @@
                     acao:'loja'
                 },
                 success:function(dados){
-                    $.alert(dados.status);
+                    console.log(dados.status);
                 }
         });
 
