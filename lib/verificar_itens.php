cd5ef7bd2ed10e407773b3da923a1d771fa56c1c
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-09 11:01:38 -0400

    Atualizações

--- lib/verificar_itens.php
+++ lib/verificar_itens.php
@@ -1,8 +1,18 @@
 <?php
     include("includes.php");
 
-    function VerificarProdutos($cod){
+    function VerificarProdutos($l=false){
         global $con;
+        global $_SESSION;
+
+        $query = "select * from vendas_produtos where venda = '{$_SESSION['AppVenda']}' and deletado != '1'";
+        $result = mysqli_query($con, $query);
+        while($d = mysqli_fetch_object($result)){
+            $cod[] = $d->produto;
+        }
+
+        if(!$cod) return false;
+
         $query = "select * from produtos where codigo in (".implode(",",$cod).")";
         $result = mysqli_query($con, $query);
 
@@ -14,12 +24,21 @@
 
             $lojas = json_decode($d->lojas);
             foreach($lojas as $i => $val){
+                //Dentro do Array status[loja][produto] = situação
                 $status[$i][$d->codigo] = $val->situacao;
             }
 
         }
 
-        return $status;
+        if($l){
+            $st = true;
+            foreach($status[$l] as $i => $v){
+                if(!$v) $st = false;
+            }
+            return ["status" => $st];
+        }else{
+            return $status;
+        }
 
         // if($status) echo "<pre>"; print_r($status); echo "</pre>";
 
@@ -27,4 +46,4 @@
 
 // print_r($_GET['cod']);
 
-    if($_GET['cod']) print_r(VerificarProdutos($_GET['cod']));
+    if($_GET['cod']) print_r(VerificarProdutos());
