f199e615c2f41e836b3426574828db2aa88e7806
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-11 10:47:20 -0400

    Atualizações

---
+++ api/bloquear_produtos.php
@@ -0,0 +1,29 @@
+<?php
+
+include("../lib/includes.php");
+
+// $query = "select codigo, nome from lojas where situacao = '1' order by nome";
+
+$query = "select
+                    a.codigo,
+                    concat(b.categoria,' - ', a.produto) as nome,
+                    JSON_EXTRACT(a.lojas,'$.\"{$_GET['cod']}\".situacao') as situacao
+            from produtos a
+                left join categorias b on a.categoria = b.codigo
+            where
+                    a.deletado != '1' and
+                    b.deletado != '1' and
+                    a.situacao = '1' and
+                    b.situacao = '1'
+            order by
+                    b.ordem asc,
+                    a.produto asc
+    ";
+
+$result = mysqli_query($con, $query);
+$dados = [];
+while($d = mysqli_fetch_object($result)){
+    $dados[] = $d;
+}
+
+echo json_encode($dados);
