6b222ee58ea7f6c4609bd34c6ee895fc99eaa8cd
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-11 15:23:02 -0400

    Atualizações

--- api/mudar_status.php
+++ api/mudar_status.php
@@ -10,14 +10,16 @@ if($_POST['pedido']){
     // $result = mysqli_query($con, $query);
 
     //ESSAS DUAS LINHAS SÃO PARA A SOLICITAÇÃO DA ENTREGA BEE
-    $BEE = new Bee;
-    $retorno = $BEE->NovaEntrega($_POST['pedido']);
-    $retorno = json_decode($retorno);
-    file_put_contents("log.txt", $retorno);
-    if($retorno->deliveryId){
-        $query = "update vendas set deliveryId = '{$retorno->deliveryId}', situacao = 'i' where codigo = '{$_POST['pedido']}'";
-        mysqli_query($con, $query);
-    }
+
+    // $BEE = new Bee;
+    // $retorno = $BEE->NovaEntrega($_POST['pedido']);
+    // $retorno = json_decode($retorno);
+    // file_put_contents("log.txt", $retorno);
+    // if($retorno->deliveryId){
+    //     $query = "update vendas set deliveryId = '{$retorno->deliveryId}', situacao = 'i' where codigo = '{$_POST['pedido']}'";
+    //     mysqli_query($con, $query);
+    // }
+    $query = "update vendas set deliveryId = '{$retorno->deliveryId}', situacao = 'i' where codigo = '{$_POST['pedido']}'";
     //////////////////////////////////////////////////////////
 
     $dados = ['status' => $query];
