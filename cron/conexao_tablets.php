54e9bca45e417f6f8d603dbfdb1530054a15f351
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-12 15:45:24 -0400

    Atualizações

--- cron/conexao_tablets.php
+++ cron/conexao_tablets.php
@@ -3,9 +3,9 @@
     include("../lib/includes.php");
 
 
-    $query = "SELECT *, date_add(ultima_conexao, interval 1 minute), if(NOW() > date_add(ultima_conexao, interval 1 minute) OR ultima_conexao = 0, '0', '1') as situacao2 FROM `lojas` where situacao = '1' and deletado != '1'";
+    echo $query = "SELECT *, date_add(ultima_conexao, interval 1 minute), if(NOW() > date_add(ultima_conexao, interval 1 minute) OR ultima_conexao = 0, '0', '1') as situacao2 FROM `lojas` where situacao = '1' and deletado != '1'";
     $result = mysqli_query($con, $query);
-
+    echo "<br><br><br><br>";
     while($d = mysqli_fetch_object($result)){
 
         echo $q1 = "UPDATE lojas set online = '{$d->online}' where codigo = '{$d->codigo}'";
