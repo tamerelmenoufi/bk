23199f739191550fef5fd4a8c6e7fc21b8b74bd6
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-12 17:35:46 -0400

    Atualizações

--- cron/conexao_tablets.php
+++ cron/conexao_tablets.php
@@ -4,15 +4,21 @@
 
     $query = "SELECT *, date_add(ultima_conexao, interval 1 minute), if(NOW() > date_add(ultima_conexao, interval 1 minute) OR ultima_conexao = 0, '0', '1') as situacao2 FROM `lojas` where situacao = '1' and deletado != '1'";
     $result = mysqli_query($con, $query);
+    $msg = false;
     while($d = mysqli_fetch_object($result)){
         $q1 = "UPDATE lojas set online = '{$d->situacao2}' where codigo = '{$d->codigo}'";
         $q2 = "INSERT INTO logs_conexoes set loja = '{$d->codigo}', data = NOW(), situacao = '{$d->situacao2}'";
         mysqli_query($con, $q1);
         mysqli_query($con, $q2);
 
-        $m = (date("i")*1);
-        if($m%5 == 0){
-            // EnviarWapp('92991886570','Lojas desligadas');
+        if(!$d->situacao2){
+            $msg .= "\nLoja {$d->nome} encontra-se desconectada ao Delivery BKManaus.";
         }
 
+    }
+
+    $m = (date("i")*1);
+    if($m%5 == 0 and $msg){
+        $msg = "*ATENÇÃO* - Notificação BKManaus".$msg;
+        EnviarWapp('92991886570',$msg);
     }
