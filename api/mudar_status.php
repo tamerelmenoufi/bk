0b64af9f68e5cff81ed929fb63624ec6f93d5501
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-12 18:00:52 -0400

    Atualizações

--- api/mudar_status.php
+++ api/mudar_status.php
@@ -11,20 +11,18 @@ echo $_POST['pedido'] = 12624;
 if($_POST['pedido']){
     // $query = "UPDATE vendas set situacao = 'i' where codigo = '{$_POST['pedido']}'";
     // $result = mysqli_query($con, $query);
-
     //ESSAS DUAS LINHAS SÃO PARA A SOLICITAÇÃO DA ENTREGA BEE
-
     $BEE = new Bee;
     $retorno = $BEE->NovaEntrega($_POST['pedido']);
     $retorno = json_decode($retorno);
     file_put_contents("log.txt", $retorno);
     if($retorno->deliveryId){
-        echo $query = "update vendas set deliveryId = '{$retorno->deliveryId}', situacao = 'i' where codigo = '{$_POST['pedido']}'";
+        $query = "update vendas set deliveryId = '{$retorno->deliveryId}', situacao = 'i' where codigo = '{$_POST['pedido']}'";
         mysqli_query($con, $query);
     }
 
     //////////////////////////////////////////////////////////
 
-    $dados = ['status' => $query];
+    $dados = ['status' => true];
 }
 echo json_encode($dados);
