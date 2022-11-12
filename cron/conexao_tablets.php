2e45b722e4df75705880d02d9ccef09660bb05ef
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-12 19:30:15 -0400

    Atualizações

--- cron/conexao_tablets.php
+++ cron/conexao_tablets.php
@@ -4,7 +4,7 @@
 
     $query = "SELECT *, date_add(ultima_conexao, interval 1 minute), if(NOW() > date_add(ultima_conexao, interval 1 minute) OR ultima_conexao = 0, '0', '1') as situacao2 FROM `lojas` where situacao = '1' and deletado != '1'";
     $result = mysqli_query($con, $query);
-    $msg = false;
+    $msg = [];
     while($d = mysqli_fetch_object($result)){
         $q1 = "UPDATE lojas set online = '{$d->situacao2}' where codigo = '{$d->codigo}'";
         $q2 = "INSERT INTO logs_conexoes set loja = '{$d->codigo}', data = NOW(), situacao = '{$d->situacao2}'";
@@ -12,7 +12,7 @@
         mysqli_query($con, $q2);
 
         if(!$d->situacao2){
-            $msg .= " Loja {$d->nome} encontra-se desconectada ao Delivery BKManaus. ";
+            $msg[] = " *{$d->nome}* ";
         }
 
     }
@@ -21,14 +21,14 @@
     $h = (date("H")*1);
 
     if($m%5 == 0 and 11 <= $h and $h <= 23 and $msg){
-        $msg = "*ATENÇÃO* - Notificação BKManaus ".$msg;
+        $msg = "*ATENÇÃO* - Notificação BKManaus. As lojas a seguir encontram-se desconectadas: ".implode(", ",$msg);
         $numeros = [
             '92991886570',
             '92988020814',
             '92984124929',
         ];
         for($i=0;$i<count($numeros);$i++){
-            // EnviarWapp($numeros[$i], $msg);
+            EnviarWapp($numeros[$i], $msg);
         }
 
     }
