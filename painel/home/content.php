73657f1e059eb0f7ad2b1ea84623e4a1364c260c
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-13 13:29:46 -0400

    Atualizações

--- painel/home/content.php
+++ painel/home/content.php
@@ -124,68 +124,30 @@ $dadosCount = mysqli_fetch_object(mysqli_query($con, $queryCount));
 
 
 <script>
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
 
-const DATA_COUNT = 7;
-const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
 
-const labels = Utils.months({count: 7});
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [10, 30, 50, 20, 25, 44, -10],
-      borderColor: Utils.CHART_COLORS.red,
-      backgroundColor: Utils.CHART_COLORS.red,
-    },
-    {
-      label: 'Dataset 2',
-      data: ['ON', 'ON', 'OFF', 'ON', 'OFF', 'OFF', 'ON'],
-      borderColor: Utils.CHART_COLORS.blue,
-      backgroundColor: Utils.CHART_COLORS.blue,
-      stepped: true,
-      yAxisID: 'y2',
-    }
-  ]
-};
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        text: 'Stacked scales',
-      },
-    },
-    scales: {
-      y: {
-        type: 'linear',
-        position: 'left',
-        stack: 'demo',
-        stackWeight: 2,
-        grid: {
-          borderColor: Utils.CHART_COLORS.red
-        }
-      },
-      y2: {
-        type: 'category',
-        labels: ['ON', 'OFF'],
-        offset: true,
-        position: 'left',
-        stack: 'demo',
-        stackWeight: 1,
-        grid: {
-          borderColor: Utils.CHART_COLORS.blue
-        }
-      }
-    }
-  },
-};
-
-
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
 const myChart = new Chart(
     document.getElementById('myChart'),
     config
