6f673836cf439324b17e474674aa7f72a6763371
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-13 17:42:40 -0400

    Atualizações

--- painel/home/content.php
+++ painel/home/content.php
@@ -168,7 +168,7 @@ const config = {
       },
       title: {
         display: true,
-        text: 'Percentual de Conectividade das lojas em <?=formata_datahora($data)?>'
+        text: 'Percentual de Conectividade das lojas em <?=formata_datahora($data,'d/m/Y')?>'
       }
     }
   },
