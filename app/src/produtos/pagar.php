57fe1d32710c574cba82a23e5ed6a789bb0f019d
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-14 09:50:18 -0400

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -272,6 +272,7 @@
                                         <?php
                                             $bee = new Bee;
                                             list($lat, $lng) = explode(",", $coordenadas);
+
                                             $q = "select * from lojas where situacao = '1' and online='1' and deletado != '1'";
                                             $r = mysqli_query($con, $q);
                                             $vlopc = 0;
