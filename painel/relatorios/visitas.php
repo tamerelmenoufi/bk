6b8261f3429a01a8d9ae7064215170d0fa69debb
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-17 10:55:27 -0400

    Atualizações

--- painel/relatorios/visitas.php
+++ painel/relatorios/visitas.php
@@ -4,12 +4,11 @@
     $query = "select
                     a.*,
                     b.nome,
-                    b.telefone,
-                    if(a.data_pedido >= '2022-11-10 00:00:00', 'green','red') as cor
+                    b.telefone
                 from vendas a
                     left join clientes b on a.cliente = b.codigo
                 where   a.deletado != '1' and
-                        (a.operadora_situacao = 'approved' or a.operadora_situacao != 'Approved')
+                        (a.operadora_situacao != 'Approved')
                 order by a.data_pedido desc";
     $result = mysqli_query($con, $query);
 ?>
@@ -33,17 +32,8 @@
     $valor = $taxa = $total = 0;
     while($d = mysqli_fetch_object($result)){
 
-        if($d->cor == 'red'){
-            $style = 'style = "color:red; text-decoration:line-through;"';
-        }else{
-            $style = 'style = "color:green;"';
-            $valor = $valor + $d->valor;
-            $taxa = $taxa + $d->taxa_entrega;
-            $total = $total + $d->total;
-        }
-
 ?>
-            <tr <?=$style?>>
+            <tr>
                 <td><?=$d->nome?></td>
                 <td><?=$d->telefone?></td>
                 <td><?=$d->data_pedido?></td>
