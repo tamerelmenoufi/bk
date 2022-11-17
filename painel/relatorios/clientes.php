d8aa6f73192640065fd601036f81e834c2ae82c3
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-17 09:16:24 -0400

    Atualizações

--- painel/relatorios/clientes.php
+++ painel/relatorios/clientes.php
@@ -1,1 +1,35 @@
-Clientes
+<?php
+    include("../../lib/includes.php");
+
+    $query = "select * from clientes";
+    $result = mysqli_query($con, $query);
+?>
+
+<div class="col">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>CPF</th>
+                <th>E-mail</th>
+                <th>Data Cadastro</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+    while($d = mysqli_fetch_object($result)){
+?>
+            <tr>
+                <td><?=$d->nome?></td>
+                <td><?=$d->Telefone?></td>
+                <td><?=$d->cpf?></td>
+                <td><?=$d->email?></td>
+                <td><?=$d->data_cadastro?></td>
+            </tr>
+<?php
+    }
+?>
+        </tbody>
+    </table>
+</div>
