18e9b79cef813941c4fb58f53968de16fa56918f
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-19 09:03:08 -0400

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -274,7 +274,7 @@
                                         </ul>
                                     </div>
 
-                                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                                    <div id="collapseOneXXXX" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                                         <ul class="list-group">
                                         <?php
                                             $bee = new Bee;
