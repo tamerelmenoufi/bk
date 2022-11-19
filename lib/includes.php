a4b4682f669c303d5ea457c6b90e272eab7415d3
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-18 21:10:19 -0400

    Atualizações

--- lib/includes.php
+++ lib/includes.php
@@ -19,7 +19,7 @@ date_default_timezone_set("America/Manaus");
 $inicio = strtotime(date("Y-m-d 11:00:00"));
 $final  = strtotime(date("Y-m-d 21:00:00"));
 $agora = strtotime("NOW");
-if($inicio <= $agora and $final >= $agora){
+if($inicio <= $agora and $agora <= $final){
     $StatusApp = 'a';
 }else{
     $StatusApp = 'f';
