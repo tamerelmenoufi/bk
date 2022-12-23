125558e111e63767c2b2a8c7ff5e18caf2e5f1ce
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-23 17:58:31 -0400

    Atualizações

--- app/src/cliente_promocao/perfil.php
+++ app/src/cliente_promocao/perfil.php
@@ -69,7 +69,7 @@
     <div class="row">
             <div class="col-12">
                 <div style="width:100%; text-align:center;bottom margin-top:30px; margin-top:10px;">
-                    <img src="img/promocao_sorteio.gif" alt="Promoção Frete Grátis" style="width:100%; border-radius:10px;" />
+                    <img src="img/promocao_sorteios.gif" alt="Promoção Frete Grátis" style="width:100%; border-radius:10px;" />
                 </div>
 
                 <div class="form-group">
