e70384e15aaf60d63b42e815290f74e623bbcc3d
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-23 21:28:51 -0400

    Atualizações

--- app/src/cliente_promocao/regulamento.php
+++ app/src/cliente_promocao/regulamento.php
@@ -1,26 +1,37 @@
 <?php
     include("../../../lib/includes.php");
 ?>
-
-<h3>Regulamento para o sorteio</3>
-
+<style>
+    .PedidoTopoTitulo{
+        position:fixed;
+        left:70px;
+        top:0px;
+        height:60px;
+        padding-top:15px;
+        z-index:1;
+    }
+</style>
+<div class="p-3">
+<div class="PedidoTopoTitulo">
+    <h4>Regulamento</h4>
+</div>
 <h4>Campanha: #EUTONABKMANAUS</h4>
 
 <p>
-Participe do Sorteio para concorrer a 6 (seis) COMBOS WHOPPER/Q por semana.<br>
-Os sorteios serão divulgados em eventos/shows e postagens nas mídias sociais<br>
-A campanha atual terá validade até o dia 24 de dezembro de 2022<br>
-Os participantes devem informar o nome completo e o número de telefone WhatsApp<br>
-A confirmação do cadastro será enviada em forma de mensagem no WhatsApp cadastrado<br>
-Na mensagem enviada será informado o número de participação no sorteio<br>
-Os sorteados deverão comprovar em qualquer uma das lojas BKMANAUS, o seu nome completo, número de telefone WhatsApp e o código fornecido na inscrição.<br>
+<li>Participe do Sorteio para concorrer a 6 (seis) COMBOS WHOPPER/Q por semana;</li><br>
+<li>Os sorteios serão divulgados em eventos/shows e postagens nas mídias sociais;</li><br>
+<li>A campanha atual terá validade até o dia 24 de dezembro de 2022;</li><br>
+<li>Os participantes devem informar o nome completo e o número de telefone WhatsApp;</li><br>
+<li>A confirmação do cadastro será enviada em forma de mensagem no WhatsApp cadastrado;</li><br>
+<li>Na mensagem enviada será informado o número de participação no sorteio;</li><br>
+<li>Os sorteados deverão comprovar em qualquer uma das lojas BKMANAUS, o seu nome completo, número de telefone WhatsApp e o código fornecido na inscrição.</li><br>
 </p>
 <p>Boa Sorte</p>
 
 <p style="text-align:center">
     <button class="btn btn-primary aceito_termos">Aceito</button>
 </p>
-
+</div>
 
 <script>
     $(function(){
