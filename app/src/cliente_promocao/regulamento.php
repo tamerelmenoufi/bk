f979fca5db10c693b79a4219df10479ba3c5a029
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-06 18:10:35 -0400

    Atualizações

--- app/src/cliente_promocao/regulamento.php
+++ app/src/cliente_promocao/regulamento.php
@@ -20,7 +20,7 @@
 <p>
 <li>Participe do Sorteio para concorrer a 6 (seis) COMBOS WHOPPER/Q por semana;</li>
 <li>Os sorteios serão divulgados em eventos/shows e postagens nas mídias sociais;</li>
-<li>A campanha atual terá validade até o dia 24 de dezembro de 2022;</li>
+<!-- <li>A campanha atual terá validade até o dia 24 de dezembro de 2022;</li> -->
 <li>Os participantes devem informar o nome completo e o número de telefone WhatsApp;</li>
 <li>A confirmação do cadastro será enviada em forma de mensagem no WhatsApp cadastrado;</li>
 <li>Na mensagem enviada será informado o número de participação no sorteio;</li>
