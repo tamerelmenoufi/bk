8fbc1fa159561faab9e0ff609d176fa4df7b8849
livia128924 <livia128924@gmail.com>
2023-02-16 10:13:56 -0300

    atualizando

--- site/index.php
+++ site/index.php
@@ -321,7 +321,7 @@
 
         <div class="row g-0">
           <div class="col-lg-6 video-play position-relative">
-            <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox play-btn"></a>
+            <a href="https://youtu.be/DyyLU2HVsRE" class="glightbox play-btn"></a>
           </div>
           <div class="col-lg-6">
             <div class="content d-flex flex-column justify-content-center h-100">
