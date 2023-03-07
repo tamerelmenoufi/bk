7b449376a80290c3a15672481b32b3a665191f57
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-07 20:46:10 +0200

    Atualizações

--- api/mudar_status.php
+++ api/mudar_status.php
@@ -12,8 +12,8 @@ if($_POST['pedido']){
     //ESSAS DUAS LINHAS SÃO PARA A SOLICITAÇÃO DA ENTREGA BEE
     $BEE = new Bee;
     $retorno = $BEE->NovaEntrega($_POST['pedido']);
-    $retorno = json_decode($retorno);
     file_put_contents("log.txt", "TESTE: ".$retorno);
+    $retorno = json_decode($retorno);
     if($retorno->deliveryId == 9999){
         $query = "update vendas set
                                     deliveryId = '{$retorno->deliveryId}',
