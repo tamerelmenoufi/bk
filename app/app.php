e258ec406fb6bda00aa7f72b5af2d5eefe6466be
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-09 22:29:44 +0200

    Atualizações

--- app/app.php
+++ app/app.php
@@ -22,7 +22,32 @@
 <h2>Lista de pedidos</h2>
 
 <?php
-    echo $query = "select * from vendas where loja = '{$_GET['l']}'";
+    $query = "SELECT
+    a.*,
+    concat( a.produto_nome, ' [', a.quantidade ,' x R$', a.valor_unitario , ' = R$' , a.valor_total, '] ' ) as produto_nome,
+    b.categoria,
+    b.descricao,
+    concat(c.prefixo,' ',c.categoria) as nome_categoria,
+    v.situacao,
+    v.SEARCHING,
+    v.GOING_TO_ORIGIN,
+    v.ARRIVED_AT_ORIGIN,
+    v.GOING_TO_DESTINATION,
+    v.ARRIVED_AT_DESTINATION,
+    v.name,
+    v.phone,
+    v.loja,
+    e.entrega_gratis,
+    cl.nome as cliente_nome,
+    cl.telefone as cliente_telefone,
+    concat(trim(e.rua), ', ',  trim(e.numero), ', ', trim(e.bairro), ', ', trim(e.complemento), ', ', trim(e.referencia)) as endereco
+        FROM vendas_produtos a
+        left join produtos b on a.produto = b.codigo
+        left join vendas v on a.venda = v.codigo
+        left join categorias c on b.categoria = c.codigo
+        left join clientes cl on v.cliente = cl.codigo
+        left join clientes_enderecos e on v.cliente = e.cliente and e.padrao = '1'
+    where v.loja = '{$_GET['l']}' and a.deletado != '1' and v.deletado != '1' and v.operadora_situacao = 'approved' order by v.data_pedido desc";
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
 ?>
