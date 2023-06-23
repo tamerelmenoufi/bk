7f96d1b544952762405f3e34f7de4269d11d64a9
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-23 03:26:47 +0300

    Atualizações

--- lib/vendor/mottu/classes.php
+++ lib/vendor/mottu/classes.php
@@ -106,7 +106,7 @@ class mottu {
 
         curl_close($curl);
 
-        return $response."\n".$this->Ambiente($this->ambiente)."\n".$this->apiKey($this->ambiente, $loja)."\n";
+        return $response; //."\n".$this->Ambiente($this->ambiente)."\n".$this->apiKey($this->ambiente, $loja)."\n";
     }
 
     public function calculaFrete($json, $loja = false){
@@ -132,7 +132,7 @@ class mottu {
 
         $response = curl_exec($curl);
         curl_close($curl);
-        return $response; //."\n".$this->Ambiente($this->ambiente)."\n".$this->apiKey($this->ambiente, $loja)."\n";
+        return $response."\n".$this->Ambiente($this->ambiente)."\n".$this->apiKey($this->ambiente, $loja)."\n";
 
     }
 
