659d8fac375e72ea6e21d44414f01cc176bba3fd
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-24 14:54:56 +0300

    Atualizações

--- lib/vendor/mottu/classes.php
+++ lib/vendor/mottu/classes.php
@@ -4,16 +4,16 @@ class mottu {
 
     public $ambiente = 'homologacao'; //homologacao ou producao
 
-    public function Ambiente($opc, $a = 'producao'){
-        if($opc == 'homologacao' and $a == 'homologacao'){
+    public function Ambiente($opc){
+        if($opc == 'homologacao'){
             return 'https://integrations.mottu.io/delivery';
         }else{
             return 'https://integrations.mottu.cloud/delivery';
         }
     }
 
-    public function apiKey($opc, $loja, $a = 'producao'){
-        if($opc == 'homologacao' and $a == 'homologacao'){
+    public function apiKey($opc, $loja){
+        if($opc == 'homologacao'){
             return 'F74C23D9DF05489E9A5185EB5F7DEE28';
         }else{
             $Lojas = [
@@ -54,12 +54,12 @@ class mottu {
     }
 
 
-    public function ConsultarPedido($pedido, $loja = false, $a = 'producao'){
+    public function ConsultarPedido($pedido, $loja = false){
 
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => $this->Ambiente($this->ambiente, $a).'/orders/'.$pedido,
+        CURLOPT_URL => $this->Ambiente($this->ambiente).'/orders/'.$pedido,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -70,7 +70,7 @@ class mottu {
         CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HTTPHEADER => array(
             'Content-Type: application/json',
-            'x-api-token: '.$this->apiKey($this->ambiente, $loja, $a),
+            'x-api-token: '.$this->apiKey($this->ambiente, $loja),
             'accept: application/json'
         ),
         ));
@@ -112,12 +112,12 @@ class mottu {
         return $response; //."\n".$this->Ambiente($this->ambiente)."\n".$this->apiKey($this->ambiente, $loja)."\n";
     }
 
-    public function calculaFrete($json, $loja = false, $a){
+    public function calculaFrete($json, $loja = false){
 
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://integrations.mottu.cloud/delivery/orders/preview",
+        CURLOPT_URL => $this->Ambiente($this->ambiente)."/orders/preview",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -129,14 +129,14 @@ class mottu {
         CURLOPT_POSTFIELDS => $json,
         CURLOPT_HTTPHEADER => array(
             'Content-Type: application/json',
-            'x-api-token: 8C0CC3BEBE314FD1830520A2A09AC8F8',
+            'x-api-token: '.$this->apiKey($this->ambiente, $loja),
             'accept: application/json'
         ),
         ));
 
         $response = curl_exec($curl);
         curl_close($curl);
-        return $response."\n".$this->Ambiente($this->ambiente, $a)."/orders/preview"."\n".$this->apiKey($this->ambiente, $loja, $a)."\n";
+        return $response; //."\n".$this->Ambiente($this->ambiente, $a)."/orders/preview"."\n".$this->apiKey($this->ambiente, $loja, $a)."\n";
 
     }
 
