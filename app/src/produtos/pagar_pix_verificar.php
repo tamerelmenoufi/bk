f67d657804fc970335d26f5ce7d96214349f865d
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-30 21:01:59 +0300

    Atualizações

--- app/src/produtos/pagar_pix_verificar.php
+++ app/src/produtos/pagar_pix_verificar.php
@@ -84,10 +84,10 @@
         $retorno1 = $mottu->NovoPedido($json, $d->id_mottu);
         $retorno = json_decode($retorno1);
 
-        $query = "update vendas set deliveryId = '{$retorno->id}', situacao = 'p', data_finalizacao = NOW() where codigo = '{$codVenda}'";
+        $query = "update vendas set deliveryId = '{$retorno['id']}', situacao = 'p', data_finalizacao = NOW() where codigo = '{$codVenda}'";
         mysqli_query($con, $query);
 
-        EnviarWapp('92991886570',"VENDA - Código do pedido (Verificar) *{$codVenda}*");
+        EnviarWapp('92991886570',"VENDA - Código do pedido (Verificar) *{$codVenda}* ID mottu: {$retorno['id']}");
         }else{
         $query = "update vendas set situacao = 'p', data_finalizacao = NOW() where codigo = '{$codVenda}'";
         mysqli_query($con, $query);
