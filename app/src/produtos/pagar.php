317d18367d644c2517a461495cf47c4c70c1a397
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-30 18:37:10 +0300

    Atualizações

--- app/src/produtos/pagar.php
+++ app/src/produtos/pagar.php
@@ -456,13 +456,13 @@ if($d->cliente == 2){
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col">
-                            <div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-  </div>
-</div>
+                            <div class="col-12">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="row" style="margin-top:10px;">
