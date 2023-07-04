663b1de6afeacd4a68a3b9a70b375bfe28929ec7
tamerelmenoufi <tamer@mohatron.com.br>
2023-07-05 00:17:09 +0300

    Atualizações

--- app/src/cliente/alert_pedido.php
+++ app/src/cliente/alert_pedido.php
@@ -1,14 +1,11 @@
 <?php
     include("../../../lib/includes.php");
 
-    $tempo = date("Y-m-d H:i:s", mktime((date("H") - 12), date("i"), date("s"), date("m"), date("d"), date("Y")));
-
     $query = "select * from vendas where
-                                    data_pedido >= '{$tempo}' and
                                     cliente = '{$_SESSION['AppCliente']}' and
-                                    forma_pagamento = 'pix' and
-                                    operadora_situacao = 'pending' and
+                                    situacao in ('p','i') and
                                     deletado != '1'
+                order by codigo desc limit 1
                                     ";
     $result = mysqli_query($con, $query);
     // $d = mysqli_fetch_object($result);
