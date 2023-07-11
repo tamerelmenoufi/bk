5bb166d4151a69c9e84973a60fa2805db1e05f21
tamerelmenoufi <tamer@mohatron.com.br>
2023-07-12 01:16:46 +0300

    Atualizações

--- api/notificacoes.php
+++ api/notificacoes.php
@@ -28,7 +28,7 @@ include("{$_SERVER['DOCUMENT_ROOT']}/bk/lib/includes.php");
         ];
         $msg[] = [
             'telefone' => '92991886570', //$d->telefone,
-            'mensagem' => $mensagem
+            'mensagem' => $mensagem." - {$d->codigo}"
         ];
         mysqli_query($con, "update vendas set notificacoes = JSON_SET(if(notificacoes > 0,notificacoes,'{}'), '$.n1', '1') where codigo = '{$d->codigo}'");
     }
@@ -39,6 +39,45 @@ include("{$_SERVER['DOCUMENT_ROOT']}/bk/lib/includes.php");
 
 
     // Visitas que preencheram o carrinho de compras
+    $query = "select
+                    a.*,
+                    b.nome,
+                    b.telefone
+                from vendas a
+                     left join clientes b on a.cliente = b.codigo
+                where
+                     data_pedido > DATE_SUB(NOW(), INTERVAL 90 MINUTE) and
+                     valor > 0 and
+                     a.operadora_situacao != '' and
+                     a.operadora_situacao != 'approved' and
+                     a.cliente > 0 and
+                     a.notificacoes->'$.n2' IS NULL
+            ";
+    $result = mysqli_query($con, $query);
+    $msg = [];
+    while($d = mysqli_fetch_object($result)){
+        $mensagem = "*BK MANAUS* - Olá {$d->nome}, ficamos muito felizes com a sua visita em nossa aplicação bkmanaus.com.br. Criamos esta plataforma para lhe oferecer as mesmas vantagens nas lojas físicas e sem precisar sair do conforto de sua casa. Falta pouco para finalizar sua compra. *Aguardamos seu pedido*";
+        echo "<br>{$d->telefone} - {$mensagem}";
+        $msg[] = [
+            'telefone' => $d->telefone,
+            'mensagem' => $mensagem
+        ];
+        $msg[] = [
+            'telefone' => '92991886570', //$d->telefone,
+            'mensagem' => $mensagem." - {$d->codigo}"
+        ];
+        mysqli_query($con, "update vendas set notificacoes = JSON_SET(if(notificacoes > 0,notificacoes,'{}'), '$.n2', '1') where codigo = '{$d->codigo}'");
+    }
+
+    foreach($msg as $ind => $val){
+        EnviarWapp($val['telefone'], $val['mensagem']);
+    }
+
+
+
+
+exit();
+    // Vendas realizadas
     $query = "select
                     a.*,
                     b.nome,
