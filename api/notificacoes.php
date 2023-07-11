9e1154f74a32d28fb6e99f3d8d7a56d0fd5d2ec1
tamerelmenoufi <tamer@mohatron.com.br>
2023-07-12 00:46:43 +0300

    Atualizações

--- api/notificacoes.php
+++ api/notificacoes.php
@@ -4,22 +4,30 @@ error_reporting(9);
 
 include("{$_SERVER['DOCUMENT_ROOT']}/bk/lib/includes.php");
 
-    echo $query = "select
+    $query = "select
                     a.*,
                     b.nome,
                     b.telefone
                 from vendas a
                      left join clientes b on a.cliente = b.codigo
                 where
-                     data_pedido > DATE_SUB(NOW(), INTERVAL 60 MINUTE) and
+                     data_pedido > DATE_SUB(NOW(), INTERVAL 20 MINUTE) and
                      valor = 0 and
                      a.cliente > 0 and
                      a.notificacoes->'$.n1' IS NULL
             ";
     $result = mysqli_query($con, $query);
+    $msg = [];
     while($d = mysqli_fetch_object($result)){
         $mensagem = "*BK MANAUS* - Olá {$d->nome}, ficamos muito felizes com a sua visita em nossa aplicação bkmanaus.com.br. Criamos esta plataforma para lhe oferecer a mesmas vantagens da loja física e sem precisar sair do conforto de sua casa. *Estamos aguardando seu pedido*";
         echo "<br>{$d->telefone} - {$mensagem}";
-
+        $msg[] = [
+            'telefone' => '92991886570', //$d->telefone,
+            'mensagem' => $mensagem
+        ];
         mysqli_query($con, "update vendas set notificacoes = JSON_SET(if(notificacoes > 0,notificacoes,'{}'), '$.n1', '1') where codigo = '{$d->codigo}'");
+    }
+
+    foreach($msg as $ind => $val){
+        EnviarWapp($val['telefone'], $val['mensagem']);
     }
