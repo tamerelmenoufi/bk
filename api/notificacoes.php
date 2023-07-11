65e856097c14675d3cbd92afe3005384c177092a
tamerelmenoufi <tamer@mohatron.com.br>
2023-07-12 00:38:09 +0300

    Atualizações

--- api/notificacoes.php
+++ api/notificacoes.php
@@ -21,5 +21,5 @@ include("{$_SERVER['DOCUMENT_ROOT']}/bk/lib/includes.php");
         $mensagem = "*BK MANAUS* - Olá {$d->nome}, ficamos muito felizes com a sua visita em nossa aplicação bkmanaus.com.br. Criamos esta plataforma para lhe oferecer a mesmas vantagens da loja física e sem precisar sair do conforto de sua casa. *Estamos aguardando seu pedido*";
         echo "<br>{$d->telefone} - {$mensagem}";
 
-        mysqli_query($con, "update vendas set notificacoes = JSON_SET(if(notificacoes > 0,notificacoes,'{}'), '$.n1', '1')");
+        mysqli_query($con, "update vendas set notificacoes = JSON_SET(if(notificacoes > 0,notificacoes,'{}'), '$.n1', '1') where codigo = '{$d->codigo}'");
     }
