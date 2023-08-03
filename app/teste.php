258f98e67a6a9611b9a347916c0db09675e9f5c7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-02 19:03:37 -0500

    Atualizações

--- app/teste.php
+++ app/teste.php
@@ -5,94 +5,94 @@
     // exit();
 
 
-    $venda = 13523;
-    $query = "select
-                    a.*,
-                    d.id as id_loja,
-                    d.mottu as id_mottu,
-                    b.nome,
-                    b.cpf,
-                    b.telefone,
-                    b.email,
-                    c.cep,
-                    c.numero,
-                    c.rua,
-                    c.bairro,
-                    c.referencia
-                from vendas a
-                    left join clientes b on a.cliente = b.codigo
-                    left join clientes_enderecos c on c.cliente = b.codigo and c.padrao = '1'
-                    left join lojas d on a.loja = d.codigo
-                where a.codigo = '{$venda}'";
+    // $venda = 13803;
+    // $query = "select
+    //                 a.*,
+    //                 d.id as id_loja,
+    //                 d.mottu as id_mottu,
+    //                 b.nome,
+    //                 b.cpf,
+    //                 b.telefone,
+    //                 b.email,
+    //                 c.cep,
+    //                 c.numero,
+    //                 c.rua,
+    //                 c.bairro,
+    //                 c.referencia
+    //             from vendas a
+    //                 left join clientes b on a.cliente = b.codigo
+    //                 left join clientes_enderecos c on c.cliente = b.codigo and c.padrao = '1'
+    //                 left join lojas d on a.loja = d.codigo
+    //             where a.codigo = '{$venda}'";
 
-                $result = mysqli_query($con, $query);
-                $d = mysqli_fetch_object($result);
+    //             $result = mysqli_query($con, $query);
+    //             $d = mysqli_fetch_object($result);
 
-                echo $json = "{
-                    \"code\": \"{$d->codigo}\",
-                    \"fullCode\": \"bk-{$d->codigo}\",
-                    \"preparationTime\": 0,
-                    \"previewDeliveryTime\": false,
-                    \"sortByBestRoute\": false,
-                    \"deliveries\": [
-                      {
-                        \"code\": \"{$d->codigo}\",
-                        \"confirmation\": {
-                          \"mottu\": true
-                        },
-                        \"name\": \"{$d->nome}\",
-                        \"phone\": \"".trim(str_replace(array(' ','-','(',')'), false, $d->telefone))."\",
-                        \"observation\": \"{$d->observacoes}\",
-                        \"address\": {
-                          \"street\": \"{$d->rua}\",
-                          \"number\": \"{$d->numeroxx}\",
-                          \"complement\": \"{$d->referencia}\",
-                          \"neighborhood\": \"{$d->bairro}\",
-                          \"city\": \"Manaus\",
-                          \"state\": \"AM\",
-                          \"zipCode\": \"".trim(str_replace(array(' ','-'), false, $d->cep))."\"
-                        },
-                        \"onlinePayment\": true,
-                        \"productValue\": {$d->total}
-                      }
-                    ]
-                  }";
+    //             echo $json = "{
+    //                 \"code\": \"{$d->codigo}\",
+    //                 \"fullCode\": \"bk-{$d->codigo}\",
+    //                 \"preparationTime\": 0,
+    //                 \"previewDeliveryTime\": false,
+    //                 \"sortByBestRoute\": false,
+    //                 \"deliveries\": [
+    //                   {
+    //                     \"code\": \"{$d->codigo}\",
+    //                     \"confirmation\": {
+    //                       \"mottu\": true
+    //                     },
+    //                     \"name\": \"{$d->nome}\",
+    //                     \"phone\": \"".trim(str_replace(array(' ','-','(',')'), false, $d->telefone))."\",
+    //                     \"observation\": \"{$d->observacoes}\",
+    //                     \"address\": {
+    //                       \"street\": \"{$d->rua}\",
+    //                       \"number\": \"{$d->numeroxx}\",
+    //                       \"complement\": \"{$d->referencia}\",
+    //                       \"neighborhood\": \"{$d->bairro}\",
+    //                       \"city\": \"Manaus\",
+    //                       \"state\": \"AM\",
+    //                       \"zipCode\": \"".trim(str_replace(array(' ','-'), false, $d->cep))."\"
+    //                     },
+    //                     \"onlinePayment\": true,
+    //                     \"productValue\": {$d->total}
+    //                   }
+    //                 ]
+    //               }";
 
-                $mottu = new mottu;
+    //             $mottu = new mottu;
 
-                $retorno1 = $mottu->NovoPedido($json, $d->id_mottu);
-                $retorno = json_decode($retorno1);
+    //             $retorno1 = $mottu->NovoPedido($json, $d->id_mottu);
+    //             $retorno = json_decode($retorno1);
 
-                print_r($retorno);
+    //             print_r($retorno);
 
-                if($retorno->id == 9999){
-                    $query = "update vendas set
-                                                deliveryId = '{$retorno->id}',
-                                                situacao = 'p',
-                                                GOING_TO_DESTINATION = NOW(),
-                                                name = 'Unidade Djalma Batista',
-                                                phone = '(92) 9843-87438'
-                            where codigo = '{$venda}'";
-                    mysqli_query($con, $query);
-                    EnviarWapp('92991886570',"VENDA - Venda do pedido *{$venda}*");
-                }else if($retorno->id){
-                    $query = "update vendas set
-                                                operadora = 'rede',
-                                                operadora_situacao = 'Approved',
-                                                data_finalizacao = NOW(),
-                                                forma_pagamento = 'credito',
-                                                deliveryId = '{$retorno->id}',
-                                                situacao = 'p'
-                                where codigo = '{$venda}'";
-                    mysqli_query($con, $query);
-                    EnviarWapp('92991886570',"VENDA - Venda do pedido *{$venda}*");
-                }else{
-                    EnviarWapp('92991886570',"VENDA - Venda do pedido *{$venda}* não gerou entrega.");
-                }
+    //             if($retorno->id == 9999){
+    //                 $query = "update vendas set
+    //                                             deliveryId = '{$retorno->id}',
+    //                                             situacao = 'p',
+    //                                             GOING_TO_DESTINATION = NOW(),
+    //                                             name = 'Unidade Djalma Batista',
+    //                                             phone = '(92) 9843-87438'
+    //                         where codigo = '{$venda}'";
+    //                 mysqli_query($con, $query);
+    //                 EnviarWapp('92991886570',"VENDA - Venda do pedido *{$venda}*");
+    //             }else if($retorno->id){
+    //                 $query = "update vendas set
+    //                                             operadora = 'rede',
+    //                                             operadora_situacao = 'Approved',
+    //                                             data_finalizacao = NOW(),
+    //                                             forma_pagamento = 'credito',
+    //                                             deliveryId = '{$retorno->id}',
+    //                                             situacao = 'p'
+    //                             where codigo = '{$venda}'";
+    //                 mysqli_query($con, $query);
+    //                 EnviarWapp('92991886570',"VENDA - Venda do pedido *{$venda}*");
+    //             }else{
+    //                 EnviarWapp('92991886570',"VENDA - Venda do pedido *{$venda}* não gerou entrega.");
+    //             }
 
 
 
-exit();
+
 
 
     echo $query = "select * from vendas_ifood where retorno < 0 limit 5";
